35d13a8ed1f8b85fc9c6f5624fb2010844b83ee5
ReallyLiri <reallyliri@gmail.com>
2022-08-15 20:25:08 +0300

    added map

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,48 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
+import "leaflet/dist/leaflet.css";
+import L from 'leaflet';
+
 
 function App() {
+
+  const [position, setPosition] = useState<GeolocationCoordinates>();
+
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition(
+      position => setPosition(position.coords)
+    )
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          🚌 Our bus is open! 🚌
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+          {
+            position && <MapContainer
+                  center={ [position.latitude, position.longitude] }
+                  zoom={ 13 }
+                  zoomControl
+                  scrollWheelZoom
+                  style={{height: "100vh", width: "100%"}}
+              >
+                  <TileLayer
+                      attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                      url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                  />
+                  <Marker position={ [position.latitude, position.longitude] }
+                  icon={L.icon({
+                    iconUrl: "./marker-icon.png",
+                  })}
+                  >
+                      <Popup>
+                          A pretty CSS3 popup. <br/> Easily customizable.
+                      </Popup>
+                  </Marker>
+              </MapContainer>
+          }
       </header>
     </div>
   );
