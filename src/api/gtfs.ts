efa8f24bc9bece79af9f1962d1c212718f451d44
Ilia Kaplounov <ilia.kaplounov@torq.io>
2022-09-12 20:37:07 +0300

    lint fixes

--- src/api/gtfs.ts
+++ src/api/gtfs.ts
@@ -1,40 +1,39 @@
-
-import { Configuration, GtfsApi } from "open-bus-stride-client";
-import agencyList from "open-bus-stride-client/agencies/agencyList";
-import moment from "moment";
+import { Configuration, GtfsApi } from 'open-bus-stride-client'
+import agencyList from 'open-bus-stride-client/agencies/agencyList'
+import moment from 'moment'
 
 //const BASE_PATH = "https://open-bus-stride-api.hasadna.org.il";
-const BASE_PATH = "http://localhost:3000/api";
-const API_CONFIG = new Configuration({basePath: BASE_PATH})
+const BASE_PATH = 'http://localhost:3000/api'
+const API_CONFIG = new Configuration({ basePath: BASE_PATH })
 const GTFS_API = new GtfsApi(API_CONFIG)
 
 export async function getStops(BASELINE_TIME: Date) {
   const routes = await GTFS_API.gtfsRoutesListGet({
-    routeShortName: "1",
-    operatorRefs: agencyList.find(value => value.agency_name === "דן")!.agency_id,
+    routeShortName: '1',
+    operatorRefs: agencyList.find((value) => value.agency_name === 'דן')!.agency_id,
     dateFrom: BASELINE_TIME,
     dateTo: BASELINE_TIME,
-    limit: 10
-  });
-  console.error(routes.map(route => [route.id, route.routeDirection, route.routeLongName]))
+    limit: 10,
+  })
+  console.error(routes.map((route) => [route.id, route.routeDirection, route.routeLongName]))
 
   const route_id = routes[0].id
   const rides = await GTFS_API.gtfsRidesListGet({
     gtfsRouteId: route_id,
-    startTimeTo: moment(BASELINE_TIME).add(60, "m").toDate(),
-    startTimeFrom: moment(BASELINE_TIME).subtract(60, "m").toDate(),
+    startTimeTo: moment(BASELINE_TIME).add(60, 'm').toDate(),
+    startTimeFrom: moment(BASELINE_TIME).subtract(60, 'm').toDate(),
     limit: 10,
-    orderBy: "start_time"
-  });
-  console.error(rides.map(ride => ride.startTime))
+    orderBy: 'start_time',
+  })
+  console.error(rides.map((ride) => ride.startTime))
 
-  const ride_id = rides[0].id?.toString();
+  const ride_id = rides[0].id?.toString()
   const stops = await GTFS_API.gtfsRideStopsListGet({
     gtfsRideIds: ride_id,
-  });
+  })
 
   for (const ride_stop of stops.sort((a, b) => a.stopSequence! - b.stopSequence!)) {
-    const stop = await GTFS_API.gtfsStopsGetGet({id: ride_stop.gtfsStopId});
+    const stop = await GTFS_API.gtfsStopsGetGet({ id: ride_stop.gtfsStopId })
     console.error(stop.name, stop.code, ride_stop.arrivalTime)
   }
 }
--- src/pages/line-page/LinePage.tsx
+++ src/pages/line-page/LinePage.tsx
@@ -5,11 +5,9 @@ import DirectionSelector from './DirectionSelector'
 import { MapContainer, Marker, Popup, TileLayer } from 'react-leaflet'
 import L, { LatLngTuple } from 'leaflet'
 import axios from 'axios'
-import { getStops } from '../../api/gtfs';
+import { getStops } from '../../api/gtfs'
 
-
-const BASELINE_TIME = new Date('2022-09-12T06:00:00Z');
-
+const BASELINE_TIME = new Date('2022-09-12T06:00:00Z')
 
 const LinePage = () => {
   const [position, setPosition] = useState<LatLngTuple>()
@@ -23,12 +21,12 @@ const LinePage = () => {
 
   useEffect(() => {
     getStops(BASELINE_TIME)
-  });
+  })
 
   const onSearch = useCallback(async () => {
     const result = await axios.get(
       'https://nominatim.openstreetmap.org/search?countrycodes=il&format=json&q=' +
-      encodeURIComponent(query),
+        encodeURIComponent(query),
     )
     const data = result.data as any[]
     console.log('search', data)
@@ -42,39 +40,39 @@ const LinePage = () => {
     <div>
       <Input
         type="text"
-        value={ query }
-        onChange={ (e) => setQuery(e.target.value) }
-        style={ {margin: '0 10px 0 10px', width: 320} }
+        value={query}
+        onChange={(e) => setQuery(e.target.value)}
+        style={{ margin: '0 10px 0 10px', width: 320 }}
       />
-      <LineSelector/>
-      <DirectionSelector/>
-      <TimePicker/>
-      <div onClick={ onSearch } style={ {color: 'blue', cursor: 'pointer'} }>
+      <LineSelector />
+      <DirectionSelector />
+      <TimePicker />
+      <div onClick={onSearch} style={{ color: 'blue', cursor: 'pointer' }}>
         SEARCH
       </div>
-      <Layout style={ {height: 600, alignItems: 'center', justifyContent: 'center'} }>
-        { position && (
+      <Layout style={{ height: 600, alignItems: 'center', justifyContent: 'center' }}>
+        {position && (
           <MapContainer
-            center={ position }
-            zoom={ 13 }
+            center={position}
+            zoom={13}
             zoomControl
             scrollWheelZoom
-            style={ {height: '100vh', width: '100%'} }>
+            style={{ height: '100vh', width: '100%' }}>
             <TileLayer
               attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
               url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
             />
             <Marker
-              position={ position }
-              icon={ L.icon({
+              position={position}
+              icon={L.icon({
                 iconUrl: './marker-icon.png',
-              }) }>
+              })}>
               <Popup>
-                A pretty CSS3 popup. <br/> Easily customizable.
+                A pretty CSS3 popup. <br /> Easily customizable.
               </Popup>
             </Marker>
           </MapContainer>
-        ) }
+        )}
       </Layout>
     </div>
   )
