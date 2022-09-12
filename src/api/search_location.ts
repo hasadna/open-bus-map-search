887c1a6900f00a22d58aa34086dce003acfb607d
ReallyLiri <reallyliri@gmail.com>
2022-09-12 20:32:50 +0300

    split line and map pages

---
+++ src/api/search_location.ts
@@ -0,0 +1,14 @@
+import axios from "axios";
+import { LatLngTuple } from "leaflet";
+
+export async function searchLocationByQueryAsync(query: string): Promise<LatLngTuple | null> {
+  const result = await axios.get(
+    'https://nominatim.openstreetmap.org/search?countrycodes=il&format=json&q=' +
+    encodeURIComponent(query),
+  )
+  const data = result.data as any[]
+  if (data.length === 0) {
+    return null
+  }
+  return [data[0]['lat'] as number, data[0]['lon'] as number]
+}
---
+++ src/pages/MapPage.tsx
@@ -0,0 +1,64 @@
+import React, { useCallback, useEffect, useState } from 'react'
+import { Input, Layout } from 'antd'
+import { MapContainer, Marker, Popup, TileLayer } from 'react-leaflet'
+import L, { LatLngTuple } from 'leaflet'
+import { searchLocationByQueryAsync } from "../api/search_location";
+
+
+const MapPage = () => {
+  const [position, setPosition] = useState<LatLngTuple>()
+  const [query, setQuery] = useState<string>('')
+
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition((position) =>
+      setPosition([position.coords.latitude, position.coords.longitude]),
+    )
+  }, [])
+
+  const onSearch = useCallback(async () => {
+    const searchResult = await searchLocationByQueryAsync(query);
+    if (searchResult) {
+      setPosition(searchResult);
+    }
+  }, [setPosition, query])
+
+  return (
+    <div>
+      <Input
+        type="text"
+        value={ query }
+        onChange={ (e) => setQuery(e.target.value) }
+        style={ {margin: '0 10px 0 10px', width: 320} }
+      />
+      <div onClick={ onSearch } style={ {color: 'blue', cursor: 'pointer'} }>
+        SEARCH
+      </div>
+      <Layout style={ {height: 600, alignItems: 'center', justifyContent: 'center'} }>
+        { position && (
+          <MapContainer
+            center={ position }
+            zoom={ 13 }
+            zoomControl
+            scrollWheelZoom
+            style={ {height: '100vh', width: '100%'} }>
+            <TileLayer
+              attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+              url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            />
+            <Marker
+              position={ position }
+              icon={ L.icon({
+                iconUrl: './marker-icon.png',
+              }) }>
+              <Popup>
+                A pretty CSS3 popup. <br/> Easily customizable.
+              </Popup>
+            </Marker>
+          </MapContainer>
+        ) }
+      </Layout>
+    </div>
+  )
+}
+
+export default MapPage
