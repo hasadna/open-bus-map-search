7ccb2c1f16f90d03d82b3945142a0eb38992bd67
ReallyLiri <reallyliri@gmail.com>
2022-10-19 12:12:50 +0300

    api path

--- src/api/apiConfig.ts
+++ src/api/apiConfig.ts
@@ -1,7 +1,7 @@
-//const BASE_PATH = "https://open-bus-stride-api.hasadna.org.il";
 import { Configuration } from 'open-bus-stride-client'
 
-const BASE_PATH = 'http://localhost:3000/api'
+//const BASE_PATH = 'http://localhost:3000/api'
+const BASE_PATH = 'https://open-bus-stride-api.hasadna.org.il'
 export const API_CONFIG = new Configuration({ basePath: BASE_PATH })
 
 export const MAX_HITS_COUNT = 16
