d15d897e2e72131ca75f470d35e0522d381800e1
ReallyLiri <liri@diversion.dev>
2023-01-26 14:20:59 +0200

    removed unused var

--- src/api/gtfsService.ts
+++ src/api/gtfsService.ts
@@ -6,7 +6,7 @@ import { API_CONFIG, MAX_HITS_COUNT } from 'src/api/apiConfig'
 import { log } from 'src/log'
 
 const GTFS_API = new GtfsApi(API_CONFIG)
-const USER_CASES_API = new UserCasesApi(API_CONFIG)
+//const USER_CASES_API = new UserCasesApi(API_CONFIG)
 const JOIN_SEPARATOR = ','
 
 export async function getRoutesAsync(
