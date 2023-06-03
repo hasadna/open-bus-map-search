9bc725d246b9432cdfce9916b48760cb0d7addd2
Noam Gaash <noam.gaash@applitools.com>
2023-06-03 16:26:07 +0300

    perf: gradually load all data

---
+++ src/api/useVehicleLocations.ts
@@ -0,0 +1,127 @@
+/**
+ * this is a custom hook that fetches the vehicle locations from the API.
+ * it recieves an interval of two dates, and loads locations of all vehicles in that interval.
+ * if some of the interval has already been loaded,
+ */
+
+import { useEffect, useState } from 'react'
+import { VehicleLocation } from 'src/model/vehicleLocation'
+
+const config = {
+  apiUrl: 'https://open-bus-stride-api.hasadna.org.il/siri_vehicle_locations/list?get_count=false',
+  limit: 500, // the maximum number of vehicles to load in one request
+  fromField: 'recorded_at_time_from',
+  toField: 'recorded_at_time_to',
+} as const
+
+type Dateable = Date | number | string
+
+function formatTime(time: Dateable) {
+  const date = new Date(time).toISOString()
+  return date
+}
+
+const loadedLocations = new Map<
+  string, // time interval
+  LocationObservable
+>()
+
+/*
+ * this class is an observable that loads the data from the API.
+ * it notifies the observers every time new data is loaded.
+ * it also caches the data, so if the same interval is requested again, it will not load it again.
+ */
+class LocationObservable {
+  constructor({ from, to }: { from: Dateable; to: Dateable }) {
+    this.#loadData({ from, to })
+  }
+
+  data: VehicleLocation[] = []
+  loading = true
+
+  async #loadData({ from, to }: { from: Dateable; to: Dateable }) {
+    let offset = 0
+    for (let i = 1; this.loading; i++) {
+      const response = await fetch(
+        `${config.apiUrl}&${config.fromField}=${formatTime(from)}&${config.toField}=${formatTime(
+          to,
+        )}&limit=${config.limit * i}&offset=${offset}`,
+      )
+      const data = await response.json()
+      if (data.length === 0) {
+        this.loading = false
+      } else {
+        this.data = [...this.data, ...data]
+        this.#notifyObservers(data)
+        offset += config.limit * i
+      }
+    }
+    this.#observers = []
+  }
+
+  #notifyObservers(data: VehicleLocation[]) {
+    const observers = this.#observers
+    console.log('notifying observers', observers.length)
+    observers.forEach((observer) => observer(data))
+  }
+
+  #observers: ((locations: VehicleLocation[]) => void)[] = []
+
+  observe(observer: (locations: VehicleLocation[]) => void) {
+    if (this.loading) {
+      this.#observers.push(observer)
+    }
+    observer(this.data)
+    return () => {
+      this.#observers = this.#observers.filter((o) => o !== observer)
+    }
+  }
+}
+
+// this function checks the cache for the data, and if it's not there, it loads it
+function getLocations(
+  from: Dateable,
+  to: Dateable,
+  update: (locations: VehicleLocation[]) => void, // the observer will be called every time with all the locations that were loaded
+) {
+  const key = `${formatTime(from)}-${formatTime(to)}`
+  if (!loadedLocations.has(key)) {
+    loadedLocations.set(key, new LocationObservable({ from, to }))
+  }
+  const observable = loadedLocations.get(key)!
+  return observable.observe(update)
+}
+
+function getMinutesInRange(from: Dateable, to: Dateable) {
+  const start = new Date(from).setSeconds(0, 0)
+  const end = new Date(to).setSeconds(0, 0)
+
+  // array of minutes to load
+  const minutes = Array.from({ length: (end - start) / 60000 }, (_, i) => ({
+    from: new Date(start + i * 60000),
+    to: new Date(start + (i + 1) * 60000),
+  }))
+  return minutes
+}
+
+export default function useVehicleLocations({ from, to }: { from: Dateable; to: Dateable }) {
+  const [locations, setLocations] = useState<VehicleLocation[]>([])
+  useEffect(() => {
+    const unmounts = getMinutesInRange(from, to).map(({ from, to }) =>
+      getLocations(from, to, (locations) => {
+        setLocations(
+          (prev) =>
+            [...prev, ...locations]
+              .sort((a, b) => a.id - b.id)
+              .filter((loc, i, arr) => arr.findIndex((l) => l.id === loc.id) === i), // remove duplicates
+        )
+      }),
+    )
+    return () => {
+      unmounts.forEach((unmount) => unmount())
+    }
+  }, [from, to])
+  return locations
+}
+
+export {}
--- src/pages/RealtimeMapPage.tsx
+++ src/pages/RealtimeMapPage.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useMemo, useState } from 'react'
 import { MapContainer, useMap, Marker, Popup, TileLayer } from 'react-leaflet'
 // import https://www.svgrepo.com/show/113626/bus-front.svg
 import busIcon from '../resources/bus-front.svg'
@@ -7,12 +7,16 @@ import MarkerClusterGroup from 'react-leaflet-cluster'
 import './Map.scss'
 import { DivIcon } from 'leaflet'
 import agencyList from 'open-bus-stride-client/agencies/agencyList'
+import useVehicleLocations from 'src/api/useVehicleLocations'
+import { VehicleLocation } from 'src/model/vehicleLocation'
 
 interface Point {
   loc: [number, number]
   color: number
   operator?: string
   bearing?: number
+  point?: VehicleLocation
+  recorded_at_time?: number
 }
 
 interface ColorMemo {
@@ -54,6 +58,11 @@ const colorIcon = ({
   }))
 }
 
+function formatTime(time: any) {
+  const date = new Date(time).toISOString()
+  return date
+}
+
 function numberToColorHsl(i: number, max: number) {
   const ratio = i / max
   // 0 - black. 1 - red
@@ -63,61 +72,60 @@ function numberToColorHsl(i: number, max: number) {
   return `hsl(${hue}, ${saturation}%, ${lightness}%)`
 }
 
-function formatTime(time: string) {
-  const date = new Date(time).toISOString()
-  return date
-}
-
 export default function RealtimeMapPage() {
   const position: Point = {
     loc: [32.3057988, 34.85478613],
     color: 0,
   }
-
-  const [positions, setPositions] = useState<Point[]>([])
-  const [limit, setLimit] = useState(3000)
-  const [loaded, setLoaded] = useState(0)
   const [from, setFrom] = useState('2023-05-01T12:00:00+02:00')
   const [to, setTo] = useState('2023-05-01T12:01:00+02:00')
 
-  useEffect(() => {
-    ;(async () => {
-      const url = `https://open-bus-stride-api.hasadna.org.il/siri_vehicle_locations/list?get_count=false&recorded_at_time_from=${
-        formatTime(from) || '2023-05-01T12:00:00+02:00'
-      }&recorded_at_time_to=${
-        formatTime(to) || '2023-05-01T12:01:00+02:00'
-      }&order_by=id%20asc&limit=${limit}`
-      const response = await fetch(url)
-      const data = await response.json()
-      const points: Point[] = data.map(
-        (point: {
-          lat: number
-          lon: number
-          velocity: number
-          siri_route__operator_ref: string
-          bearing: number
-        }) => ({
-          loc: [point.lat, point.lon],
-          color: point.velocity,
-          point,
-          operator: point.siri_route__operator_ref,
-          bearing: point.bearing,
-        }),
-      )
-      setPositions(points)
-      setLoaded(points.length)
-    })()
-  }, [limit])
+  const locations = useVehicleLocations({
+    from: formatTime(from),
+    to: formatTime(to),
+  })
+
+  const loaded = locations.length
+
+  const minTime = new Date(from).getTime()
+  const maxTime = new Date(to).getTime()
+  const [chosenTime, setChosenTime] = useState((minTime + maxTime) / 2)
+
+  const positions = useMemo(() => {
+    let pos = locations.map<Point>((location) => ({
+      loc: [location.lat, location.lon],
+      color: location.velocity,
+      operator: String(location.siri_route__operator_ref),
+      bearing: location.bearing,
+      recorded_at_time: new Date(location.recorded_at_time).getTime(),
+      point: location,
+    }))
+    // keep only the latest point for each vehicle
+    pos = pos.filter((p) =>
+      pos.every(
+        (p2) =>
+          p2.point!.siri_ride__vehicle_ref !== p.point!.siri_ride__vehicle_ref ||
+          p2.recorded_at_time! <= p.recorded_at_time!,
+      ),
+    )
+    return pos
+  }, [locations])
 
   return (
     <div className="map-container">
       <div className="map-header">
         <h1>Realtime Map</h1>
-        <div className="map-header-buttons">
-          <button onClick={() => setLimit(5000)}>5000</button>
-          <button onClick={() => setLimit(10000)}>10000</button>
-          <button onClick={() => setLimit(10000)}>30000</button>
-        </div>
+        {/* <div className="map-header-buttons">
+          <input
+            type="range"
+            min={minTime}
+            max={maxTime}
+            value={chosenTime}
+            onChange={(e) => setChosenTime(Number(e.target.value))}
+          />
+          <button onClick={() => setChosenTime(minTime)}>Start</button>
+          <button onClick={() => setChosenTime(maxTime)}>End</button>
+        </div> */}
         <div className="map-header-buttons">
           <label>
             מתאריך
@@ -129,9 +137,7 @@ export default function RealtimeMapPage() {
             <input type="datetime-local" value={to} onChange={(e) => setTo(e.target.value)} />
           </label>
         </div>
-        <p>
-          Loaded {loaded} out of {limit} points
-        </p>
+        <p>Loaded {loaded} busses</p>
       </div>
       <div className="map-info">
         <MapContainer center={position.loc} zoom={8} scrollWheelZoom={true}>
