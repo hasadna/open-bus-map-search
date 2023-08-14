e97e7f49496203bab300588119e5b8ce43a6cb18
Shay <shay@bionicstork.com>
2023-08-14 20:40:03 +0300

    add analytics per page

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,9 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
 import App from './App'
+import ReactGA from 'react-ga'
+
+ReactGA.initialize('G-0YRQT80GG1')
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 root.render(
