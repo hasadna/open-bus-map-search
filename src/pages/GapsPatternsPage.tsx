e110d71db519c3f1672c16873fe8f8e176795bc0
Shay <shay@bionicstork.com>
2023-09-16 19:23:46 +0300

    order by support

--- src/pages/GapsPatternsPage.tsx
+++ src/pages/GapsPatternsPage.tsx
@@ -27,6 +27,7 @@ import {
   ComposedChart,
   Cell,
 } from 'recharts'
+import { FormControlLabel, Radio, RadioGroup, Switch } from '@mui/material'
 
 // Define prop types for the component
 interface BusLineStatisticsProps {
@@ -43,6 +44,161 @@ interface HourlyData {
 }
 
 const now = moment()
+
+function GapsByHour({ lineRef, operatorRef, fromDate, toDate }: BusLineStatisticsProps) {
+  const [hourlyData, setHourlyData] = useState<HourlyData[]>([])
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const gapsList = await getGapsAsync(fromDate, toDate, operatorRef, lineRef)
+
+        // Convert gapsList data into hourly mapping as needed
+        const hourlyMapping: Record<string, { planned_rides: number; actual_rides: number }> = {}
+
+        for (const ride of gapsList) {
+          if (ride.gtfsTime === null) {
+            continue
+          }
+          const plannedHour = ride.gtfsTime.format('HH:mm')
+
+          if (!hourlyMapping[plannedHour]) {
+            hourlyMapping[plannedHour] = { planned_rides: 0, actual_rides: 0 }
+          }
+
+          hourlyMapping[plannedHour].planned_rides += 1
+          if (ride.siriTime) {
+            hourlyMapping[plannedHour].actual_rides += 1
+          }
+        }
+
+        const result: HourlyData[] = Object.entries(hourlyMapping).map(([hour, data]) => ({
+          planned_hour: hour,
+          actual_rides: data.actual_rides,
+          planned_rides: data.planned_rides,
+        }))
+
+        result.sort((a, b) => a.planned_hour.localeCompare(b.planned_hour))
+        setHourlyData(result)
+      } catch (error) {
+        console.error('Error fetching data:', error)
+      }
+    }
+
+    fetchData()
+  }, [lineRef, operatorRef, fromDate, toDate])
+
+  const getColorForMisses = (planned: number, actual: number) => {
+    const misses = planned - actual
+    const percentageMisses = (misses / planned) * 100
+
+    if (percentageMisses <= 5) {
+      return 'green'
+    } else if (percentageMisses <= 50) {
+      return 'orange'
+    } else {
+      return 'red'
+    }
+  }
+
+  const maxPlannedRides = Math.max(
+    ...hourlyData.map((entry) => entry.planned_rides),
+    ...hourlyData.map((entry) => entry.actual_rides),
+  )
+
+  const [sortingMode, setSortingMode] = useState<'hour' | 'severity'>('hour')
+
+  // Function to sort the data array by hour
+  const sortByHour = () => {
+    hourlyData.sort((a, b) => a.planned_hour.localeCompare(b.planned_hour))
+  }
+
+  const sortBySeverity = () => {
+    hourlyData.sort((a, b) => {
+      const missesA = a.planned_rides - a.actual_rides
+      const missesB = b.planned_rides - b.actual_rides
+      const percentageMissesA = (missesA / a.planned_rides) * 100
+      const percentageMissesB = (missesB / b.planned_rides) * 100
+
+      // First, compare by percentage misses
+      if (percentageMissesA !== percentageMissesB) {
+        return percentageMissesB - percentageMissesA // Reverse order here
+      }
+
+      // If percentage misses are equal, compare by the amount of misses
+      return missesB - missesA // Reverse order here
+    })
+  }
+
+  // Sort the data based on the current sorting mode
+  if (sortingMode === 'hour') {
+    sortByHour()
+  } else if (sortingMode === 'severity') {
+    sortBySeverity()
+  }
+
+  return (
+    <div>
+      <div>
+        <RadioGroup
+          row
+          aria-label="sorting-mode"
+          name="sorting-mode"
+          value={sortingMode}
+          onChange={(e) => setSortingMode(e.target.value as 'hour' | 'severity')}>
+          <FormControlLabel value="hour" control={<Radio />} label="Sort by Hour" />
+          <FormControlLabel value="severity" control={<Radio />} label="Sort by Severity" />
+        </RadioGroup>
+      </div>
+      <ComposedChart
+        layout="vertical"
+        width={500}
+        height={hourlyData.length * 50}
+        data={hourlyData}
+        margin={{
+          top: 20,
+          right: 20,
+          bottom: 20,
+          left: 20,
+        }}
+        barGap={-20}>
+        <CartesianGrid stroke="#f5f5f5" />
+        <XAxis
+          type="number"
+          xAxisId={0}
+          reversed={true}
+          orientation={'top'}
+          domain={[0, maxPlannedRides]}
+        />
+        <XAxis
+          type="number"
+          xAxisId={1}
+          reversed={true}
+          orientation={'top'}
+          domain={[0, maxPlannedRides]}
+          hide
+        />
+        <YAxis
+          dataKey="planned_hour"
+          type="category"
+          orientation={'right'}
+          style={{ direction: 'ltr', marginTop: '-10px' }}
+        />
+        <Tooltip />
+        <Legend />
+        <Bar dataKey="actual_rides" barSize={20} radius={9} xAxisId={1} opacity={30}>
+          {hourlyData.map((entry, index) => (
+            <Cell
+              key={`cell-${index}`}
+              fill={getColorForMisses(entry.planned_rides, entry.actual_rides)}
+            />
+          ))}
+        </Bar>
+        <Bar dataKey="planned_rides" barSize={20} fill="#413ea055" radius={9} xAxisId={0} />
+      </ComposedChart>
+    </div>
+  )
+}
 
 const GapsPatternsPage = () => {
   const [startDate, setStartDate] = UseDate(now.clone().subtract(7, 'days'))
@@ -129,117 +285,6 @@ const GapsPatternsPage = () => {
         fromDate={startDate}
         toDate={endDate}></GapsByHour>
     </PageContainer>
-  )
-}
-
-function GapsByHour({ lineRef, operatorRef, fromDate, toDate }: BusLineStatisticsProps) {
-  const [hourlyData, setHourlyData] = useState<HourlyData[]>([])
-
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const gapsList = await getGapsAsync(fromDate, toDate, operatorRef, lineRef)
-
-        // Convert gapsList data into hourly mapping as needed
-        const hourlyMapping: Record<string, { planned_rides: number; actual_rides: number }> = {}
-
-        for (const ride of gapsList) {
-          if (ride.gtfsTime === null) {
-            continue
-          }
-          const plannedHour = ride.gtfsTime.format('HH:mm')
-
-          if (!hourlyMapping[plannedHour]) {
-            hourlyMapping[plannedHour] = { planned_rides: 0, actual_rides: 0 }
-          }
-
-          hourlyMapping[plannedHour].planned_rides += 1
-          if (ride.siriTime) {
-            hourlyMapping[plannedHour].actual_rides += 1
-          }
-        }
-
-        const result: HourlyData[] = Object.entries(hourlyMapping).map(([hour, data]) => ({
-          planned_hour: hour,
-          actual_rides: data.actual_rides,
-          planned_rides: data.planned_rides,
-        }))
-
-        result.sort((a, b) => a.planned_hour.localeCompare(b.planned_hour))
-        setHourlyData(result)
-      } catch (error) {
-        console.error('Error fetching data:', error)
-      }
-    }
-
-    fetchData()
-  }, [lineRef, operatorRef, fromDate, toDate])
-
-  const getColorForMisses = (planned: number, actual: number) => {
-    const misses = planned - actual
-    const percentageMisses = (misses / planned) * 100
-
-    if (percentageMisses <= 5) {
-      return 'green'
-    } else if (percentageMisses <= 50) {
-      return 'orange'
-    } else {
-      return 'red'
-    }
-  }
-
-  const maxPlannedRides = Math.max(
-    ...hourlyData.map((entry) => entry.planned_rides),
-    ...hourlyData.map((entry) => entry.actual_rides),
-  )
-
-  return (
-    <ComposedChart
-      layout="vertical"
-      width={500}
-      height={hourlyData.length * 50}
-      data={hourlyData}
-      margin={{
-        top: 20,
-        right: 20,
-        bottom: 20,
-        left: 20,
-      }}
-      barGap={-20}>
-      <CartesianGrid stroke="#f5f5f5" />
-      <XAxis
-        type="number"
-        xAxisId={0}
-        reversed={true}
-        orientation={'top'}
-        domain={[0, maxPlannedRides]}
-      />
-      <XAxis
-        type="number"
-        xAxisId={1}
-        reversed={true}
-        orientation={'top'}
-        domain={[0, maxPlannedRides]}
-        hide
-      />
-      <YAxis
-        dataKey="planned_hour"
-        type="category"
-        orientation={'right'}
-        style={{ direction: 'ltr', marginTop: '-10px' }}
-      />
-      <Tooltip />
-      <Legend />
-      <Bar dataKey="actual_rides" barSize={20} radius={9} xAxisId={1} opacity={30}>
-        {hourlyData.map((entry, index) => (
-          <Cell
-            key={`cell-${index}`}
-            fill={getColorForMisses(entry.planned_rides, entry.actual_rides)}
-          />
-        ))}
-      </Bar>
-      <Bar dataKey="planned_rides" barSize={20} fill="#413ea055" radius={9} xAxisId={0} />
-    </ComposedChart>
   )
 }
 
