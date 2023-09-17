49f7295c3c4e1ddac9bebd54147c4b09ccbeafcc
Noam Gaash <noam.gaash@applitools.com>
2023-09-17 21:36:01 +0300

    ci: allways get traces

--- playwright.config.ts
+++ playwright.config.ts
@@ -27,7 +27,7 @@ export default defineConfig({
     // baseURL: 'http://127.0.0.1:3000',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-    trace: 'on-first-retry',
+    trace: process.env.CI ? 'on' : 'on-all-retries',
   },
 
   /* Configure projects for major browsers */
