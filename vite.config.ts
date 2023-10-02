dbff4e227298e9a2c3f0aaec286cbf879eb93920
Rajat Kumar Gupta <22871961+knightcube@users.noreply.github.com>
2023-10-02 12:34:36 +0530

    Fixed errors while replacing CRA with Vite

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,10 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/',
-  plugins: [react()]
+  plugins: [react()],
+  resolve: {
+    alias: {
+      src: "/src",
+    },
+  },
 })
