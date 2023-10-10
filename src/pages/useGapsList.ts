7ca1f607b635bda4e50c8e26e8bece9bc2313255
moshe vilner <shootermv@gmail.com>
2023-10-10 14:40:16 +0300

    refactor: move sortByMode to utils

--- src/pages/useGapsList.ts
+++ src/pages/useGapsList.ts
@@ -2,7 +2,8 @@ import { useEffect, useState } from 'react'
 import { Moment } from 'moment'
 import { getGapsAsync } from '../api/gapsService'
 
-import { HourlyData, byHourHandler, bySeverityHandler } from './components/utils'
+import { sortByMode, HourlyData } from './components/utils'
+import { GapsList } from 'src/model/gaps'
 
 export const useGapsList = (
   fromDate: Moment,
@@ -10,22 +11,13 @@ export const useGapsList = (
   operatorRef: string,
   lineRef: number,
   sortingMode: string,
-) => {
+): HourlyData[] => {
   const [hourlyData, setHourlyData] = useState<HourlyData[]>([])
 
-  const sortData = (hourlyData: HourlyData[] = [], sortingMode: string) => {
-    const orderedData = [...hourlyData]
-    if (sortingMode === 'hour') {
-      orderedData.sort(byHourHandler)
-    } else if (sortingMode === 'severity') {
-      orderedData.sort(bySeverityHandler)
-    }
-    return orderedData
-  }
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const gapsList = await getGapsAsync(fromDate, toDate, operatorRef, lineRef)
+        const gapsList: GapsList = await getGapsAsync(fromDate, toDate, operatorRef, lineRef)
 
         // Convert gapsList data into hourly mapping as needed
         const hourlyMapping: Record<string, { planned_rides: number; actual_rides: number }> = {}
@@ -50,11 +42,9 @@ export const useGapsList = (
           planned_hour: hour,
           actual_rides: data.actual_rides,
           planned_rides: data.planned_rides,
-        }))
+        })) 
 
-        const orderedData = sortData(result, sortingMode)
-
-        setHourlyData(orderedData)
+        setHourlyData(sortByMode(result, sortingMode))
       } catch (error) {
         console.error('Error fetching data:', error)
       }
