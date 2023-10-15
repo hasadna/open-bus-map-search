ef620c6d25c02b0967e4976ebbebdafa245c7789
giladfuchs <gf@cortexmg.com>
2023-10-15 17:45:28 +0300

    add scss file and remove all using of sx

--- src/pages/components/MapLayers/BusToolTip.tsx
+++ src/pages/components/MapLayers/BusToolTip.tsx
@@ -3,6 +3,7 @@ import { Point } from 'src/pages/RealtimeMapPage'
 import { Button, Grid } from '@mui/material'
 import { DivIcon } from 'leaflet'
 import moment from 'moment-timezone'
+import './BusToolTip.scss'
 
 import { getSiriRideWithRelated } from 'src/api/siriService'
 import { SiriRideWithRelatedPydanticModel } from 'open-bus-stride-client/openapi/models/SiriRideWithRelatedPydanticModel'
@@ -27,7 +28,7 @@ export function BusToolTip({ position, icon }: { position: Point; icon: DivIcon 
       .finally(() => setIsLoading(false))
   }, [position])
   return (
-    <div style={showJson ? { minWidth: '30rem', background: 'white' } : {}}>
+    <div className={showJson ? 'extend-for-json' : ''}>
       {isLoading ? (
         <Row>
           <Label text={TEXTS.loading_routes} />
@@ -40,31 +41,31 @@ export function BusToolTip({ position, icon }: { position: Point; icon: DivIcon 
           {siriRide && JSON.stringify(siriRide, null, 2)}
         </pre>
       ) : (
-        <Grid container display="flex" direction="row-reverse" justifyContent="space-between">
+        <Grid container className="tool-tip-container">
           <Grid item xs={4}>
-            <h4 style={{ alignContent: 'end' }}>
+            <h4 className="bus-icon">
               <div dangerouslySetInnerHTML={{ __html: icon.options.html as string }} />
             </h4>
           </Grid>
           <Grid item xs={8} mt={2}>
-            <h4 style={{ textAlign: 'start' }}>
+            <h4>
               {TEXTS.line} :<span>{siriRide && siriRide!.gtfsRouteRouteShortName}</span>
             </h4>
           </Grid>
           <Grid item xs={12}>
-            <h4 style={{ textAlign: 'start' }}>
+            <h4>
               {TEXTS.from} :
               <span>{siriRide && siriRide!.gtfsRouteRouteLongName?.split('<->')[0]}</span>
             </h4>
-            <h4 style={{ textAlign: 'start' }}>
+            <h4>
               {TEXTS.destination} :
               <span>{siriRide && siriRide!.gtfsRouteRouteLongName?.split('<->')[1]}</span>
             </h4>
-            <h4 style={{ textAlign: 'start' }}>
+            <h4>
               {TEXTS.velocity} :<span>{`${position.point?.velocity}  ${TEXTS.kmh}`}</span>
             </h4>
 
-            <h4 style={{ textAlign: 'start' }}>
+            <h4>
               {TEXTS.sample_time} :
               <span>
                 {moment(position.point!.recorded_at_time as string, moment.ISO_8601)
@@ -72,19 +73,19 @@ export function BusToolTip({ position, icon }: { position: Point; icon: DivIcon 
                   .format('HH:mm')}
               </span>
             </h4>
-            <h4 style={{ textAlign: 'start' }}>
+            <h4>
               {TEXTS.vehicle_ref} :<span>{position.point?.siri_ride__vehicle_ref}</span>
             </h4>
             {/*maybe option to add info like this in extend card for now I  put this condition */}
             {window.screen.height > 1100 && (
               <>
-                <h4 style={{ textAlign: 'start' }}>
+                <h4>
                   {TEXTS.drive_direction} :
                   <span>
                     ( {position.point?.bearing} {TEXTS.bearing})
                   </span>
                 </h4>
-                <h4 style={{ textAlign: 'start' }}>
+                <h4>
                   {TEXTS.coords} :<span>{position.loc.join(' , ')}</span>
                 </h4>
               </>
@@ -92,7 +93,7 @@ export function BusToolTip({ position, icon }: { position: Point; icon: DivIcon 
           </Grid>{' '}
         </Grid>
       )}
-      <Grid item xs={3} mt={1} mb={1} sx={{ float: 'left' }}>
+      <Grid item xs={3} mt={1} mb={1} className="button">
         <Button
           variant="contained"
           onClick={() => {
