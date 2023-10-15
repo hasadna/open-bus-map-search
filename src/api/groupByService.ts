8e8207da07e2ce09ee6b403d1cc0be76b578fd78
moshe vilner <shootermv@gmail.com>
2023-10-15 20:14:19 +0300

    refactor: improve convert structure logic

--- src/api/groupByService.ts
+++ src/api/groupByService.ts
@@ -48,6 +48,21 @@ export type GroupByResponse = {
   route_long_name: string
 }[]
 
+export type GroupByRes = Replace<
+  GroupByResponse[0],
+  'operator_ref',
+  | {
+      agency_id: string
+      agency_name: string
+      agency_url: string
+      agency_timezone: string
+      agency_lang: string
+      agency_phone: string
+      agency_fare_url: string
+    }
+  | undefined
+>
+
 async function groupbyAsync({
   dateTo,
   dateFrom,
