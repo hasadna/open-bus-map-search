f20b7d024a6c81e988d7c7852a430afc0a3dce08
Noam Gaash <noam.gaash@applitools.com>
2023-10-19 12:21:49 +0300

    feat: add retries

--- src/complaint/complaint.js
+++ src/complaint/complaint.js
@@ -33,53 +33,60 @@ await context.tracing.start({
 })
 const page = await context.newPage()
 try {
-  await page.goto(url)
-  console.log('page loaded')
-  await page.click('text=כניסה לטופס')
-  await page.getByLabel('שם פרטי').fill(complaint.firstName)
-  await page.getByLabel('שם משפחה').fill(complaint.lastName)
-  await page.getByLabel('מספר זהות').fill(complaint.id)
-  await page.getByLabel('טלפון נייד', { exact: true }).fill(complaint.phone)
-  await page.getByRole('textbox', { name: 'דואר אלקטרוני' }).fill(complaint.email)
+  for (let attempt = 0; attempt < 3; attempt++) {
+    try {
+      await page.goto(url)
+      console.log('page loaded')
+      await page.click('text=כניסה לטופס')
+      await page.getByLabel('שם פרטי').fill(complaint.firstName)
+      await page.getByLabel('שם משפחה').fill(complaint.lastName)
+      await page.getByLabel('מספר זהות').fill(complaint.id)
+      await page.getByLabel('טלפון נייד', { exact: true }).fill(complaint.phone)
+      await page.getByRole('textbox', { name: 'דואר אלקטרוני' }).fill(complaint.email)
 
-  console.log('filled first page')
+      console.log('filled first page')
 
-  await page.getByRole('button', { name: 'לשלב הבא' }).click()
-  //   await page.getByLabel('נושא הפנייה').getByLabel('בחירה מהרשימה').first().click()
-  //   await new Promise((r) => setTimeout(r, 100))
-  await page.getByLabel('נושא הפנייה', { exact: true }).fill('אוטובוס')
-  await new Promise((r) => setTimeout(r, 100))
-  await page.getByLabel('נושא הפנייה').getByLabel('בחירה מהרשימה').first().click()
-  await new Promise((r) => setTimeout(r, 100))
-  await page.getByText('אוטובוס', { exact: true }).click()
-  await new Promise((r) => setTimeout(r, 100))
-  await page.getByLabel('סוג הפנייה').fill(complaintTypes[0])
-  await new Promise((r) => setTimeout(r, 100))
-  await page.getByText(complaintTypes[0]).click()
-  await new Promise((r) => setTimeout(r, 100))
+      await page.getByRole('button', { name: 'לשלב הבא' }).click()
+      //   await page.getByLabel('נושא הפנייה').getByLabel('בחירה מהרשימה').first().click()
+      //   await new Promise((r) => setTimeout(r, 100))
+      await page.getByLabel('נושא הפנייה', { exact: true }).fill('אוטובוס')
+      await new Promise((r) => setTimeout(r, 100))
+      await page.getByLabel('נושא הפנייה').getByLabel('בחירה מהרשימה').first().click()
+      await new Promise((r) => setTimeout(r, 100))
+      await page.getByText('אוטובוס', { exact: true }).click()
+      await new Promise((r) => setTimeout(r, 100))
+      await page.getByLabel('סוג הפנייה').fill(complaintTypes[0])
+      await new Promise((r) => setTimeout(r, 100))
+      await page.getByText(complaintTypes[0]).click()
+      await new Promise((r) => setTimeout(r, 100))
 
-  console.log('filled second page')
+      console.log('filled second page')
 
-  await page.getByRole('button', { name: 'לשלב הבא' }).click()
-  await page.getByLabel('חברת האוטובוסים (מפעיל)').fill(complaint.operator)
-  await page.getByRole('textbox', { name: 'מספר רישוי' }).fill(complaint.licensePlate)
-  await page.getByRole('textbox', { name: 'תאריך האירוע' }).fill(formatDate(complaint.time))
-  await page.getByRole('textbox', { name: 'שעת האירוע' }).fill(formatTime(complaint.time))
-  await page.getByLabel('שעת המתנה מ').fill('00:00')
-  await page.getByLabel('שעת המתנה עד').fill('23:59')
-  await page
-    .getByRole('textbox', { name: 'תוכן הפנייה' })
-    .fill('האוטובוס עצר בתחנה ולא נסע עד שעה מאוחרת')
+      await page.getByRole('button', { name: 'לשלב הבא' }).click()
+      await page.getByLabel('חברת האוטובוסים (מפעיל)').fill(complaint.operator)
+      await page.getByRole('textbox', { name: 'מספר רישוי' }).fill(complaint.licensePlate)
+      await page.getByRole('textbox', { name: 'תאריך האירוע' }).fill(formatDate(complaint.time))
+      await page.getByRole('textbox', { name: 'שעת האירוע' }).fill(formatTime(complaint.time))
+      await page.getByLabel('שעת המתנה מ').fill('00:00')
+      await page.getByLabel('שעת המתנה עד').fill('23:59')
+      await page
+        .getByRole('textbox', { name: 'תוכן הפנייה' })
+        .fill('האוטובוס עצר בתחנה ולא נסע עד שעה מאוחרת')
 
-  await page.getByRole('textbox', { name: 'מספר קו' }).fill(complaint.lineNumber)
-  await page.getByLabel('מוצא/ יעד (כיוון )').fill(complaint.routeName)
+      await page.getByRole('textbox', { name: 'מספר קו' }).fill(complaint.lineNumber)
+      await page.getByLabel('מוצא/ יעד (כיוון )').fill(complaint.routeName)
 
-  console.log('filled third page')
+      console.log('filled third page')
 
-  for (let i = 0; i < 10 && (await page.getByText('הנחיות לצירוף מסמכים').isHidden()); i++) {
-    // retry until it works
-    await page.getByRole('button', { name: 'לשלב הבא' }).click()
-    await page.waitForTimeout(300)
+      for (let i = 0; i < 10 && (await page.getByText('הנחיות לצירוף מסמכים').isHidden()); i++) {
+        // retry until it works
+        await page.getByRole('button', { name: 'לשלב הבא' }).click()
+        await page.waitForTimeout(300)
+      }
+      break
+    } catch {
+      console.log(`attempt ${attempt} failed`)
+    }
   }
   //   await new Promise((r) => setTimeout(r, 1000000))
   //   await page.getByRole('button', { name: 'לשלב הבא' }).click({ force: true })
