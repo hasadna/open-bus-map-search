a2300c38287bd16667eb6d7cfba9bd9e11dd013b
Noam Gaash <noam.gaash@applitools.com>
2023-10-21 17:15:03 +0300

    test: use advancedRouteFromHAR

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,15 +1,42 @@
 import { Page } from '@playwright/test'
 import { test } from './utils'
 
-test('search bus station', async ({ page }) => {
+test('search bus station', async ({ page, advancedRouteFromHAR }) => {
   test.slow()
-  await page.routeFromHAR('tests/example.har', {
+  await advancedRouteFromHAR('tests/example.har', {
     url: /^(?!.*\.(ts|js|mjs)$).*api/,
     update: false,
     updateContent: 'embed',
     notFound: 'abort',
+    matcher: (request, entry) => {
+      const reqUrl = new URL(request.url())
+      const entryUrl = new URL(entry.request.url)
+      for (const param of [...reqUrl.searchParams.keys()]) {
+        if (param.includes('time') || param.includes('date')) {
+          reqUrl.searchParams.delete(param)
+          entryUrl.searchParams.delete(param)
+        }
+      }
+      if (reqUrl.searchParams.has('gtfs_ride_ids')) {
+        reqUrl.searchParams.set(
+          'gtfs_ride_ids',
+          reqUrl.searchParams.get('gtfs_ride_ids')!.split(',').sort().join(','),
+        )
+      }
+      if (entryUrl.searchParams.has('gtfs_ride_ids')) {
+        entryUrl.searchParams.set(
+          'gtfs_ride_ids',
+          entryUrl.searchParams.get('gtfs_ride_ids')!.split(',').sort().join(','),
+        )
+      }
+      const isSearchParamSame =
+        [...reqUrl.searchParams.keys()].every((key) => {
+          return reqUrl.searchParams.get(key) === entryUrl.searchParams.get(key)
+        }) && reqUrl.searchParams.size === entryUrl.searchParams.size
+      return reqUrl.pathname === entryUrl.pathname && isSearchParamSame ? 1 : -1
+    },
   })
-  await mockAPI(page)
+  // await mockAPI(page)
   await resetPage(page)
 
   await page.goto('/dashboard')
@@ -75,40 +102,6 @@ function resetPage(page: Page) {
         display: none !important;
       }
     `
-    })
-  })
-}
-
-// this function makes sure that the API is mocked using the HAR file
-async function mockAPI(page: Page) {
-  await page.route(/siri_vehicle_locations\/list/, (route) => {
-    console.log('mocking siri_vehicle_locations/list')
-    route.fallback({
-      url: 'https://open-bus-stride-api.hasadna.org.il/siri_vehicle_locations/list?limit=1024&recorded_at_time_from=2023-09-05T04%3A18%3A00.000Z&recorded_at_time_to=2023-09-05T08%3A18%3A00.000Z&lon__greater_or_equal=34.992476634299&lon__lower_or_equal=35.003177365701006&lat__greater_or_equal=32.8099453919704&lat__lower_or_equal=32.81893860802958&order_by=distance_from_siri_ride_stop_meters%20desc&siri_routes__ids=609',
-    })
-  })
-  await page.route(/gtfs_rides\/list/, (route) => {
-    console.log('mocking gtfs_rides/list')
-    route.fallback({
-      url: 'https://open-bus-stride-api.hasadna.org.il/gtfs_rides/list?limit=1&gtfs_route_id=3457449&start_time_from=2023-09-04T06%3A18%3A00.000Z&start_time_to=2023-09-06T06%3A18%3A00.000Z&order_by=start_time',
-    })
-  })
-  await page.route(/siri_rides\/list/, (route) => {
-    console.log('mocking siri_rides/list')
-    route.fallback({
-      url: 'https://open-bus-stride-api.hasadna.org.il/siri_rides/list?limit=1&gtfs_route__date_from=2023-09-05&gtfs_route__date_to=2023-09-05&gtfs_route__operator_refs=3&gtfs_route__route_short_name=10&gtfs_route__route_direction=3&gtfs_ride__gtfs_route_id=3457449&gtfs_ride__start_time_from=2023-09-04T06%3A18%3A00.000Z&gtfs_ride__start_time_to=2023-09-06T06%3A18%3A00.000Z&scheduled_start_time_from=2023-09-04T06%3A18%3A00.000Z&scheduled_start_time_to=2023-09-06T06%3A18%3A00.000Z',
-    })
-  })
-  await page.route(/siri_vehicle_locations\/list/, (route) => {
-    console.log('mocking siri_vehicle_locations/list')
-    route.fallback({
-      url: 'https://open-bus-stride-api.hasadna.org.il/siri_vehicle_locations/list?limit=1024&recorded_at_time_from=2023-09-05T04%3A18%3A00.000Z&recorded_at_time_to=2023-09-05T08%3A18%3A00.000Z&lon__greater_or_equal=34.992476634299&lon__lower_or_equal=35.003177365701006&lat__greater_or_equal=32.8099453919704&lat__lower_or_equal=32.81893860802958&order_by=distance_from_siri_ride_stop_meters%20desc&siri_routes__ids=609',
-    })
-  })
-  await page.route(/gtfs_ride_stops\/list/, (route) => {
-    console.log('mocking gtfs_ride_stops/list')
-    route.fallback({
-      url: 'https://open-bus-stride-api.hasadna.org.il/gtfs_ride_stops/list?gtfs_stop_ids=16771761&gtfs_ride_ids=52603978%2C52553909%2C52603977%2C52536914%2C52536913%2C52603979%2C52526687%2C52603980%2C52526688%2C52526686%2C52557660',
     })
   })
 }
--- tests/utils.ts
+++ tests/utils.ts
@@ -1,7 +1,3 @@
 import { test as base } from 'playwright-advanced-har'
 
-export const test = base.extend({
-  page: async ({ page }, use) => {
-    await use(page)
-  },
-})
+export const test = base
