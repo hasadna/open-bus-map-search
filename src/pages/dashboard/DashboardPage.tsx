29042049d08cda2de7eafe01086b9ba82a73fad2
ArkadiK94 <arkadi29081994@gmail.com>
2023-10-22 16:39:42 +0300

    [Bug] Fix Dashboard Layout (also in Mobile)
    What: Fixed the dashboard Layout
    Why: better ui
    How: Grid v2 of mui

--- src/pages/dashboard/DashboardPage.tsx
+++ src/pages/dashboard/DashboardPage.tsx
@@ -9,10 +9,12 @@ import { DatePicker } from '@mui/x-date-pickers/DatePicker'
 import moment from 'moment'
 import LinesHbarChart from './LineHbarChart/LinesHbarChart'
 import { FormControlLabel, Switch, Tooltip } from '@mui/material'
-import { Label } from 'src/pages/components/Label'
 import OperatorSelector from 'src/pages/components/OperatorSelector'
 import { useDate } from '../components/DateTimePicker'
 import { Skeleton } from 'antd'
+import Grid from '@mui/material/Unstable_Grid2' // Grid version 2
+import { Label } from '../components/Label'
+
 const now = moment()
 
 const convertToChartCompatibleStruct = (arr: GroupByRes[]) => {
@@ -74,66 +76,102 @@ const DashboardPage = () => {
 
   return (
     <PageContainer>
-      <div className="date-picker-container">
-        <DatePicker
-          value={startDate}
-          onChange={(data) => setStartDate(data)}
-          format="DD/MM/YYYY"
-          label={TEXTS.start}
-        />
-        -
-        <DatePicker
-          value={endDate}
-          onChange={(data) => setEndDate(data)}
-          format="DD/MM/YYYY"
-          label={TEXTS.end}
-        />
-        <FormControlLabel
-          control={
-            <Switch checked={groupByHour} onChange={(e) => setGroupByHour(e.target.checked)} />
-          }
-          label={TEXTS.group_by_hour_tooltip_content}
-        />
-        <Label text={TEXTS.choose_operator} />
-        <OperatorSelector operatorId={operatorId} setOperatorId={setOperatorId} />
-      </div>
-      <div className="widgets-container">
-        <div className="widget">
-          <h2 className="title">
-            {TEXTS.dashboard_page_title}
-            <Tooltip
-              title={convertLineFeedToHtmlTags(TEXTS.dashboard_tooltip_content)}
-              placement="left"
-              arrow>
-              <span className="tooltip-icon">i</span>
-            </Tooltip>
-          </h2>
-          {groupByOperatorLoading ? (
-            <Skeleton active />
-          ) : (
-            <OperatorHbarChart operators={convertToChartCompatibleStruct(groupByOperatorData)} />
-          )}
-        </div>
-        <div className="widget">
-          <h2 className="title">{TEXTS.worst_lines_page_title}</h2>
-          {lineDataLoading ? (
-            <Skeleton active />
-          ) : (
-            <LinesHbarChart
-              lines={convertToWorstLineChartCompatibleStruct(groupByLineData, operatorId)}
-              operators_whitelist={['אלקטרה אפיקים', 'דן', 'מטרופולין', 'קווים', 'אגד']}
+      <Grid
+        container
+        spacing={2}
+        alignItems="center"
+        sx={{ marginTop: '20px' }}
+        justifyContent="space-between">
+        <Grid lg={6} xs={12} container spacing={2} alignItems="center">
+          <Grid xs={4.5}>
+            <DatePicker
+              value={startDate}
+              onChange={(data) => setStartDate(data)}
+              format="DD/MM/YYYY"
+              label={TEXTS.start}
+              sx={{ width: '100%' }}
             />
-          )}
-        </div>
-        <div className="widget">
-          <h2 className="title">{TEXTS.dashboard_page_graph_title}</h2>
-          {loadingGrap ? (
-            <Skeleton active />
-          ) : (
-            <ArrivalByTimeChart data={convertToGraphCompatibleStruct(graphData)} />
-          )}
-        </div>
-      </div>
+          </Grid>
+          <Grid xs={0.1}>-</Grid>
+          <Grid xs={4.5}>
+            <DatePicker
+              value={endDate}
+              onChange={(data) => setEndDate(data)}
+              format="DD/MM/YYYY"
+              label={TEXTS.end}
+              sx={{ width: '100%' }}
+            />
+          </Grid>
+          <Grid xs={1}>
+            <FormControlLabel
+              control={
+                <Switch checked={groupByHour} onChange={(e) => setGroupByHour(e.target.checked)} />
+              }
+              label={TEXTS.group_by_hour_tooltip_content}
+            />
+          </Grid>
+        </Grid>
+        <Grid lg={1} display={{ xs: 'none', lg: 'block' }}>
+          <Label text={TEXTS.choose_operator} />
+        </Grid>
+        <Grid lg={5} display={{ xs: 'none', lg: 'block' }}>
+          <OperatorSelector
+            operatorId={operatorId}
+            setOperatorId={setOperatorId}
+            onlyMajorOperators
+          />
+        </Grid>
+      </Grid>
+      <Grid container spacing={2} alignItems="flex-start">
+        <Grid xs={12} lg={6}>
+          <div className="widget">
+            <h2 className="title">
+              {TEXTS.dashboard_page_title}
+              <Tooltip
+                title={convertLineFeedToHtmlTags(TEXTS.dashboard_tooltip_content)}
+                placement="left"
+                arrow>
+                <span className="tooltip-icon">i</span>
+              </Tooltip>
+            </h2>
+            {groupByOperatorLoading ? (
+              <Skeleton active />
+            ) : (
+              <OperatorHbarChart operators={convertToChartCompatibleStruct(groupByOperatorData)} />
+            )}
+          </div>
+        </Grid>
+        <Grid xs={6} display={{ xs: 'block', lg: 'none' }}>
+          <OperatorSelector
+            operatorId={operatorId}
+            setOperatorId={setOperatorId}
+            onlyMajorOperators
+          />
+        </Grid>
+        <Grid xs={12} lg={6}>
+          <div className="widget">
+            <h2 className="title">{TEXTS.worst_lines_page_title}</h2>
+            {lineDataLoading ? (
+              <Skeleton active />
+            ) : (
+              <LinesHbarChart
+                lines={convertToWorstLineChartCompatibleStruct(groupByLineData, operatorId)}
+                operators_whitelist={['אלקטרה אפיקים', 'דן', 'מטרופולין', 'קווים', 'אגד']}
+              />
+            )}
+          </div>
+        </Grid>
+        <Grid xs={12}>
+          <div className="widget">
+            <h2 className="title">{TEXTS.dashboard_page_graph_title}</h2>
+            {loadingGrap ? (
+              <Skeleton active />
+            ) : (
+              <ArrivalByTimeChart data={convertToGraphCompatibleStruct(graphData)} />
+            )}
+          </div>
+        </Grid>
+      </Grid>
     </PageContainer>
   )
 }
