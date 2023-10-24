65e3e0116925eb1c93edbb6f625f4bba6801140d
Nastia1409 <anastasia.koifman@gmail.com>
2023-10-24 10:12:14 +0300

    updated HbarChart style (#152)
    Co-authored-by: Anastasia Koifman <akoifman@outbrain.com>

--- src/pages/dashboard/HbarChart/HbarChart.tsx
+++ src/pages/dashboard/HbarChart/HbarChart.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { TEXTS } from 'src/resources/texts'
 import './HbarChart.scss'
-import cn from 'classnames'
+import { Tooltip } from '@mui/material'
 
 type Entry = { name: string; total: number; actual: number; color?: string }
 const numberFormatter = new Intl.NumberFormat('he-IL')
@@ -16,43 +16,51 @@ export function HbarChart({
   const percents = entries
     .map((o) => (o.actual / o.total) * 100)
     .map((p) => (complement ? Math.max(100 - p, 0) : p))
-  const width = percents.map((p) => Math.max(Math.min(p, 100), 0))
 
   const rows = entries.map((entry, idx) => ({
-    width: width[idx],
     percent: percents[idx],
     ...entry,
   }))
   console.log(rows[0])
+
+  const wrapWithTooltip = (element: React.ReactElement, entry: Entry) => {
+    return (
+      <Tooltip
+        placement={'top'}
+        title={
+          <div style={{ fontSize: 15 }}>
+            {TEXTS.rides_planned}: {numberFormatter.format(entry.total)}
+            <br />
+            {TEXTS.rides_actual}: {numberFormatter.format(entry.actual)}
+          </div>
+        }
+        followCursor={true}>
+        {element}
+      </Tooltip>
+    )
+  }
 
   return (
     <div className="HbarChart chart">
       {rows.map((entry) => {
         return (
           !!entry.percent && (
-            <div className="entry" key={entry.name} title={entry.name}>
-              <div className="entryName" style={{ backgroundColor: entry.color }}>
+            <div className="entry" key={entry.name}>
+              <div className="entryName" style={{ borderBottomColor: entry.color }}>
                 {entry.name.split('|').slice(0, 2).join(' | ')}
               </div>
+
               <div className="entryBar">
-                <div className="entryBarTotal">
+                {wrapWithTooltip(
                   <div
-                    className={cn('entryBarActual', { small: entry.percent < 20 })}
-                    style={{
-                      width: `${entry.width}%`,
-                      backgroundColor: entry.color,
-                    }}>
-                    {entry.percent.toFixed(2)}%
-                  </div>
-                </div>
-              </div>
-              <div className="overlay">
-                <div className="entryTotal">
-                  {TEXTS.rides_planned}: {numberFormatter.format(entry.total)}
-                </div>
-                <div className="entryActual">
-                  {TEXTS.rides_actual}: {numberFormatter.format(entry.actual)}
-                </div>
+                    className="barDisplay"
+                    style={{ width: `${entry.percent}%`, backgroundColor: entry.color }}></div>,
+                  entry,
+                )}
+                {wrapWithTooltip(
+                  <div className="entryBarTotal">{entry.percent.toFixed(2)}%</div>,
+                  entry,
+                )}
               </div>
             </div>
           )
