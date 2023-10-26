7859346cf15603ca441c615579e46eed375b523b
moshe vilner <shootermv@gmail.com>
2023-10-26 16:02:34 +0300

    refactor: use ant icon for menu toggle

--- src/pages/components/header/sidebar/SideBar.tsx
+++ src/pages/components/header/sidebar/SideBar.tsx
@@ -2,18 +2,22 @@ import React, { useState } from 'react'
 import Menu, { MenuPage } from '../menu/Menu'
 import cn from 'classnames'
 import './sidebar.scss'
+import { MenuOutlined } from '@ant-design/icons'
+
+const SidebarToggle = ({ open, setOpen }: { open: boolean; setOpen: (open: boolean) => void }) => (
+  <div className="sidebar-menu-toggle" onClick={() => setOpen(!open)}>
+    <MenuOutlined />
+  </div>
+)
+
+const Logo = () => <h1 className={'sidebar-logo'}>דאטאבוס</h1>
 
 export default function SideBar({ pages }: { pages: MenuPage[] }) {
   const [open, setOpen] = useState(false)
-
   return (
     <aside className={cn('sidebar', { open })}>
-      <div className="sidebar-menu-toggle" onClick={() => setOpen(!open)}>
-        <div className="sidebar-menu-toggle-line" />
-        <div className="sidebar-menu-toggle-line" />
-        <div className="sidebar-menu-toggle-line" />
-      </div>
-      <h1>דאטאבוס</h1>
+      <Logo />
+      <SidebarToggle open={open} setOpen={setOpen} />
       <Menu pages={pages} />
     </aside>
   )
