c88fb2ee6d6f7acf537ebf5cb2397100a283b39f
amabelleS <amabelletr@gmail.com>
2023-10-31 21:26:19 +0200

    name test, '/' as url

--- tests/realtimemap.spec.ts
+++ tests/realtimemap.spec.ts
@@ -1,7 +1,7 @@
 import { test } from '@playwright/test'
 
-test('test', async ({ page }) => {
-  await page.goto('http://localhost:3000')
+test('realtime-map page', async ({ page }) => {
+  await page.goto('/')
   await page.getByText('מפה בזמן אמת').click()
   await page.getByLabel('בחירת תאריך, התאריך שנבחר הוא').click()
   await page.getByRole('gridcell', { name: '1', exact: true }).click()
