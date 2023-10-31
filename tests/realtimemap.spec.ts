0cd3b7c498d02bfc1e0580aec963770477b52ea5
amabelleS <amabelletr@gmail.com>
2023-10-31 21:29:56 +0200

    deleted last line, unnecessary key press

--- tests/realtimemap.spec.ts
+++ tests/realtimemap.spec.ts
@@ -6,5 +6,4 @@ test('realtime-map page', async ({ page }) => {
   await page.getByLabel('בחירת תאריך, התאריך שנבחר הוא').click()
   await page.getByRole('gridcell', { name: '1', exact: true }).click()
   await page.getByLabel('דקות').fill('6')
-  await page.getByLabel('דקות').press('Enter')
 })
