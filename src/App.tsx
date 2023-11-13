a4844dae8f0da4bd81311d6ed15b580afc72fb66
VSciFlight <119410516+VSciFlight@users.noreply.github.com>
2023-11-13 14:35:46 +0200

    Update App.tsx
    deleted comment

--- src/App.tsx
+++ src/App.tsx
@@ -74,10 +74,6 @@ const PAGES = [
     label: TEXT_KEYS.about_title,
     key: '/about',
   },
-  // {
-  //   label: TEXT_KEYS.profile_page,
-  //   key: '/profile',
-  // },
   {
     label: TEXT_KEYS.report_a_bug_title,
     key: 'https://github.com/hasadna/open-bus-map-search/issues',
