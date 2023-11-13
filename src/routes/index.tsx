76067c5c66bd3048e0a0296fc3e0467fa4446495
KazemHe <kazemhe1@gmail.com>
2023-11-13 17:35:58 +0200

    lazy load -pages

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,6 +1,7 @@
 import { TEXT_KEYS } from 'src/resources/texts'
 import { Navigate, Route, Routes } from 'react-router-dom'
-import { lazy, Suspense } from 'react';
+import React, { lazy, Suspense } from 'react';
+
 const DashboardPage = lazy(() => import('../pages/dashboard/DashboardPage'));
 const TimelinePage = lazy(() => import('../pages/TimelinePage'));
 const GapsPage = lazy(() => import('../pages/GapsPage'));
@@ -9,7 +10,6 @@ const RealtimeMapPage = lazy(() => import('../pages/RealtimeMapPage'));
 const SingleLineMapPage = lazy(() => import('../pages/SingleLineMapPage'));
 const About = lazy(() => import('../pages/About'));
 import CircularProgress from '@mui/material/CircularProgress';
-
 import {
   RadarChartOutlined,
   BellOutlined,
@@ -22,92 +22,80 @@ import {
   LineChartOutlined,
 } from '@ant-design/icons'
 
-import { MenuPage } from '../pages/components/header/menu/Menu'
-
-export const PAGES: MenuPage[] = [
+export const PAGES = [
   {
     label: TEXT_KEYS.dashboard_page_title,
-    key: '/dashboard',
+    path: '/dashboard',
     icon: LaptopOutlined,
+    element: <DashboardPage />,
   },
   {
     label: TEXT_KEYS.timeline_page_title,
-    key: '/timeline',
+    path: '/timeline',
     searchParamsRequired: true,
     icon: FieldTimeOutlined,
+    element: <TimelinePage />,
   },
   {
     label: TEXT_KEYS.gaps_page_title,
-    key: '/gaps',
+    path: '/gaps',
     searchParamsRequired: true,
     icon: BarChartOutlined,
+    element: <GapsPage />,
   },
   {
     label: TEXT_KEYS.gaps_patterns_page_title,
-    key: '/gaps_patterns',
+    path: '/gaps_patterns',
     icon: LineChartOutlined,
+    element: <GapsPatternsPage />,
   },
   {
     label: TEXT_KEYS.realtime_map_page_title,
-    key: '/map',
+    path: '/map',
     icon: HeatMapOutlined,
+    element: <RealtimeMapPage />,
   },
   {
     label: TEXT_KEYS.singleline_map_page_title,
-    key: '/single-line-map',
+    path: '/single-line-map',
     searchParamsRequired: true,
     icon: RadarChartOutlined,
+    element: <SingleLineMapPage />,
   },
   {
     label: TEXT_KEYS.about_title,
-    key: '/about',
+    path: '/about',
     icon: BellOutlined,
+    element: <About />,
   },
   {
     label: TEXT_KEYS.report_a_bug_title,
-    key: 'https://github.com/hasadna/open-bus-map-search/issues',
+    path: 'https://github.com/hasadna/open-bus-map-search/issues',
     icon: BugOutlined,
+    element: null,
   },
   {
     label: TEXT_KEYS.donate_title,
-    key: 'https://www.jgive.com/new/he/ils/donation-targets/3268#donation-modal',
+    path: 'https://www.jgive.com/new/he/ils/donation-targets/3268#donation-modal',
     icon: DollarOutlined,
+    element: null,
   },
-] as MenuPage[]
+]
 
 const RoutesList = () => {
-  const RedirectToDashboard = () => <Navigate to={PAGES[0].key} replace />
+  const RedirectToDashboard = () => <Navigate to={PAGES[0].path} replace />;
+  const routes = PAGES.filter((r) => r.element);
 
   return (
     <Suspense fallback={<CircularProgress />}>
       <Routes>
-        {PAGES.map((page) => (
-          <Route key={page.key} path={page.key} element={getPageElement(page)} />
+        {routes.map(({ path, element }) => (
+          <Route key={path} path={path} element={element} />
         ))}
         <Route path="*" element={<RedirectToDashboard />} />
       </Routes>
     </Suspense>
   );
+};
 
-}
-const getPageElement = (page: MenuPage) => {
-  switch (page.key) {
-    case '/dashboard':
-      return <DashboardPage />;
-    case '/timeline':
-      return <TimelinePage />;
-    case '/gaps':
-      return <GapsPage />;
-    case '/gaps_patterns':
-      return <GapsPatternsPage />;
-    case '/map':
-      return <RealtimeMapPage />;
-    case '/single-line-map':
-      return <SingleLineMapPage />;
-    case '/about':
-      return <About />;
-    default:
-      return null;
-  }
-}
 export default RoutesList
