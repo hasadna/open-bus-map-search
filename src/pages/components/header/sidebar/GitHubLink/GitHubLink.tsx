276367c4622deab1ad6712873d897cff6d98a45b
Or Malka <ormalka1234@gmail.com>
2023-11-15 19:34:57 +0200

    Texts update for i18n

--- src/pages/components/header/sidebar/GitHubLink/GitHubLink.tsx
+++ src/pages/components/header/sidebar/GitHubLink/GitHubLink.tsx
@@ -1,11 +1,14 @@
 import React from 'react'
 import { GithubOutlined } from '@ant-design/icons'
 import { TEXT_KEYS } from 'src/resources/texts'
+import { useTranslation } from 'react-i18next'
 import './GitHubLink.scss'
 
 export default function GitHubLink() {
+  const { t } = useTranslation()
+
   const data = {
-    label: TEXT_KEYS.github_link,
+    label: t(TEXT_KEYS.github_link),
     path: 'https://github.com/hasadna/open-bus-map-search',
     icon: <GithubOutlined />,
     element: null,
