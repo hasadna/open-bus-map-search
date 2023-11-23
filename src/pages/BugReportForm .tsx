0aa030d178b5352e6fc887e5ad8a4976cdc5ce14
Or Malka <ormalka1234@gmail.com>
2023-11-23 13:20:14 +0200

    Bug report form update

--- src/pages/BugReportForm .tsx
+++ src/pages/BugReportForm .tsx
@@ -1,13 +1,10 @@
-import React from 'react'
+import React, { useState } from 'react';
 import { TEXT_KEYS } from 'src/resources/texts'
 import { useTranslation } from 'react-i18next'
 import { Form, Input, Button, Upload, message } from 'antd'
 import { UploadOutlined } from '@ant-design/icons'
+import axios from 'axios'
 import './BugReportForm.scss'
-
-interface BugReportFormProps {
-  onSubmit: (values: BugReportFormData) => void
-}
 
 interface BugReportFormData {
   title: string
@@ -16,20 +13,32 @@ interface BugReportFormData {
   expectedBehavior: string
   actualBehavior: string
   reproducibility: string
-  attachments: any[] // Adjust the type based on your requirements
+  attachments: any[] 
   contactName: string
   contactEmail: string
 }
 
-const BugReportForm: React.FC<BugReportFormProps> = ({ onSubmit }) => {
+const BugReportForm: React.FC = () => {
   const { t } = useTranslation()
   const [form] = Form.useForm()
+  const [fileList, setFileList] = useState<any[]>([])
 
   const onFinish = async (values: BugReportFormData) => {
     try {
-      onSubmit(values)
+      // Send the bug report data to the server
+      // Include fileList in the values if needed
+
+      // const response = await axios.post('http://localhost:3001/create-issue', {
+      //   ...values,
+      //   attachments: fileList,
+      // })
+
+      console.log(values)
+
+      // Handle the server response
       message.success('Bug report submitted successfully!')
       form.resetFields()
+      setFileList([]) // Reset fileList after submission
     } catch (error) {
       console.error('Error submitting bug report:', error)
       message.error('Error submitting bug report. Please try again.')
@@ -38,6 +47,10 @@ const BugReportForm: React.FC<BugReportFormProps> = ({ onSubmit }) => {
 
   const onFinishFailed = (errorInfo: any) => {
     console.log('Failed:', errorInfo)
+  }
+
+  const onFileChange = (info: any) => {
+    setFileList(info.fileList)
   }
 
   return (
@@ -111,7 +124,11 @@ const BugReportForm: React.FC<BugReportFormProps> = ({ onSubmit }) => {
         </Form.Item>
 
         <Form.Item label={t(TEXT_KEYS.bug_attachments)} name="attachments">
-          <Upload beforeUpload={() => false} listType="picture">
+          <Upload
+            beforeUpload={() => false}
+            listType="picture"
+            fileList={fileList}
+            onChange={onFileChange}>
             <Button icon={<UploadOutlined />}>{t(TEXT_KEYS.bug_attachments_upload_button)}</Button>
           </Upload>
         </Form.Item>
