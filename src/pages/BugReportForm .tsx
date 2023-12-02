80ab84d3479b47eb4549e947e706fc7315a7badb
amabelleS <amabelletr@gmail.com>
2023-12-02 17:38:40 +0200

    removed TEXT_KEYS from BugReportForm

--- src/pages/BugReportForm .tsx
+++ src/pages/BugReportForm .tsx
@@ -1,5 +1,4 @@
 import React, { useState } from 'react'
-import { TEXT_KEYS } from 'src/resources/texts'
 import { useTranslation } from 'react-i18next'
 import { Form, Input, Button, Upload, message, Select, FormProps } from 'antd'
 import { UploadOutlined } from '@ant-design/icons'
@@ -62,7 +61,7 @@ const BugReportForm: React.FC = () => {
     <div className="bug-report-form-container">
       <h1 className="logo">דאטאבוס</h1>
 
-      <span> {t(TEXT_KEYS.bug_form_description)} </span>
+      <span> {t('bug_form_description')} </span>
       <br />
       <br />
 
@@ -74,88 +73,88 @@ const BugReportForm: React.FC = () => {
         labelCol={{ span: 6 }}
         wrapperCol={{ span: 18 }}>
         <Form.Item
-          label={t(TEXT_KEYS.bug_type)}
+          label={t('bug_type')}
           name="type"
           initialValue={selectedType}
-          rules={[{ required: true, message: t(TEXT_KEYS.bug_type_message) }]}>
+          rules={[{ required: true, message: t('bug_type_message') }]}>
           <Select onChange={(value) => setSelectedType(value)}>
-            <Option value="bug">{t(TEXT_KEYS.bug_type_bug)}</Option>
-            <Option value="feature">{t(TEXT_KEYS.bug_type_feature)}</Option>
+            <Option value="bug">{t('bug_type_bug')}</Option>
+            <Option value="feature">{t('bug_type_feature')}</Option>
           </Select>
         </Form.Item>
 
         <Form.Item
-          label={t(TEXT_KEYS.bug_title)}
+          label={t('bug_title')}
           name="title"
-          rules={[{ required: true, message: t(TEXT_KEYS.bug_title_message) }]}>
+          rules={[{ required: true, message: t('bug_title_message') }]}>
           <Input />
         </Form.Item>
 
         <Form.Item
-          label={t(TEXT_KEYS.bug_contact_name)}
+          label={t('bug_contact_name')}
           name="contactName"
-          rules={[{ required: true, message: t(TEXT_KEYS.bug_contact_name_message) }]}>
+          rules={[{ required: true, message: t('bug_contact_name_message') }]}>
           <Input />
         </Form.Item>
 
         <Form.Item
-          label={t(TEXT_KEYS.bug_contact_email)}
+          label={t('bug_contact_email')}
           name="contactEmail"
           rules={[
-            { required: true, message: t(TEXT_KEYS.bug_contact_email_message) },
+            { required: true, message: t('bug_contact_email_message') },
             // { type: 'email', message: t(TEXT_KEYS.bug_contact_email_valid_message) },
           ]}>
           <Input />
         </Form.Item>
 
         <Form.Item
-          label={t(TEXT_KEYS.bug_description)}
+          label={t('bug_description')}
           name="description"
-          rules={[{ required: true, message: t(TEXT_KEYS.bug_description_message) }]}>
+          rules={[{ required: true, message: t('bug_description_message') }]}>
           <Input.TextArea rows={4} />
         </Form.Item>
 
         <Form.Item
-          label={t(TEXT_KEYS.bug_environment)}
+          label={t('bug_environment')}
           name="environment"
-          rules={[{ required: true, message: t(TEXT_KEYS.bug_environment_message) }]}>
+          rules={[{ required: true, message: t('bug_environment_message') }]}>
           <Input />
         </Form.Item>
 
         <Form.Item
-          label={t(TEXT_KEYS.bug_expected_behavior)}
+          label={t('bug_expected_behavior')}
           name="expectedBehavior"
-          rules={[{ required: true, message: t(TEXT_KEYS.bug_expected_behavior_message) }]}>
+          rules={[{ required: true, message: t('bug_expected_behavior_message') }]}>
           <Input.TextArea rows={4} />
         </Form.Item>
 
         <Form.Item
-          label={t(TEXT_KEYS.bug_actual_behavior)}
+          label={t('bug_actual_behavior')}
           name="actualBehavior"
-          rules={[{ required: true, message: t(TEXT_KEYS.bug_actual_behavior_message) }]}>
+          rules={[{ required: true, message: t('bug_actual_behavior_message') }]}>
           <Input.TextArea rows={4} />
         </Form.Item>
 
         <Form.Item
-          label={t(TEXT_KEYS.bug_reproducibility)}
+          label={t('bug_reproducibility')}
           name="reproducibility"
-          rules={[{ required: true, message: t(TEXT_KEYS.bug_reproducibility_message) }]}>
+          rules={[{ required: true, message: t('bug_reproducibility_message') }]}>
           <Input />
         </Form.Item>
 
-        <Form.Item label={t(TEXT_KEYS.bug_attachments)} name="attachments">
+        <Form.Item label={t('bug_attachments')} name="attachments">
           <Upload
             beforeUpload={() => false}
             listType="picture"
             fileList={fileList}
             onChange={onFileChange}>
-            <Button icon={<UploadOutlined />}>{t(TEXT_KEYS.bug_attachments_upload_button)}</Button>
+            <Button icon={<UploadOutlined />}>{t('bug_attachments_upload_button')}</Button>
           </Upload>
         </Form.Item>
 
         <Form.Item wrapperCol={{ offset: 4, span: 16 }}>
           <Button type="primary" htmlType="submit">
-            {t(TEXT_KEYS.bug_submit)}{' '}
+            {t('bug_submit')}{' '}
           </Button>
         </Form.Item>
       </Form>
