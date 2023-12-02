2e23f1e48ae3d66ba291631129471ee3ef5d1233
amabelleS <amabelletr@gmail.com>
2023-12-02 21:21:21 +0200

    removed PAGES from SideBar

--- src/layout/sidebar/SideBar.tsx
+++ src/layout/sidebar/SideBar.tsx
@@ -5,7 +5,7 @@ import { useContext, useState } from 'react'
 import { LayoutContextInterface, LayoutCtx } from '../LayoutContext'
 import GitHubLink from './GitHubLink/GitHubLink'
 import { Link } from 'react-router-dom'
-import { PAGES as pages } from 'src/routes'
+import { usePages } from 'src/routes'
 const { Sider } = Layout
 
 const Logo = () => (
@@ -18,6 +18,7 @@ const CollapsedLogo = () => <h1 className={'sidebar-logo-collapsed'}>🚌</h1>
 export default function SideBar() {
   const { drawerOpen, setDrawerOpen } = useContext<LayoutContextInterface>(LayoutCtx)
   const [collapsed, setCollapsed] = useState(false)
+  const pages = usePages()
   return (
     <>
       <Drawer
