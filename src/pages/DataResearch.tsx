7a0966426f99cb4a71da2a40687595cd40fb3bf3
Noam Gaash <noam.gaash@applitools.com>
2023-12-15 20:12:07 +0200

    feat: add more charts and description

--- src/pages/DataResearch.tsx
+++ src/pages/DataResearch.tsx
@@ -26,52 +26,79 @@ const unique: (value: string, index: number, self: string[]) => boolean = (value
 export const DataResearch = () => {
   return (
     <PageContainer>
-      <h1>מחקרים</h1>
-      <StackedResearchChart />
+      <Widget>
+        <h1>מחקרים</h1>
+        <p>אם יש לכם רעיון מעניין למה קורים פה דברים, דברו איתנו בסלאק!</p>
+      </Widget>
+      <StackedResearchSection />
     </PageContainer>
   )
 }
 
-export const StackedResearchChart = () => {
+function StackedResearchSection() {
   const [startDate, setStartDate] = useDate(now.clone().subtract(7, 'days'))
   const [endDate, setEndDate] = useDate(now.clone().subtract(1, 'day'))
   const [groupByHour, setGroupByHour] = React.useState<boolean>(false)
-  const { t } = useTranslation()
-
   const [graphData, loadingGraph] = useGroupBy({
     dateTo: endDate,
     dateFrom: startDate,
     groupBy: groupByHour ? 'operator_ref,gtfs_route_hour' : 'operator_ref,gtfs_route_date',
   })
 
-  const data = useMemo(
-    () =>
-      graphData.reduce((acc, curr) => {
-        if (curr.total_actual_rides === 0) return acc
-        const date = curr.gtfs_route_date ?? curr.gtfs_route_hour
-        const entry = acc.find((item) => item.date === date)
-        if (entry) {
-          entry[curr.operator_ref?.agency_name || 'Unknown'] = curr.total_actual_rides
-        } else {
-          const newEntry = {
-            date: date,
-            [curr.operator_ref?.agency_name || 'Unknown']: curr.total_actual_rides,
-          }
-          acc.push(newEntry)
-        }
-        return acc
-      }, [] as Record<string, string | number>[]),
-    [graphData],
-  )
-
-  const operators = graphData
-    .map((operator) => operator.operator_ref?.agency_name || 'Unknown')
-    .filter(unique)
-
   return (
     <Widget>
+      <StackedResearchInputs
+        startDate={startDate}
+        setStartDate={setStartDate}
+        endDate={endDate}
+        setEndDate={setEndDate}
+        groupByHour={groupByHour}
+        setGroupByHour={setGroupByHour}
+      />
       <h2>בעיות etl/gps/משהו גלובאלי אחר</h2>
-      <p>אם יש לכם רעיון מעניין למה קורים פה דברים, דברו איתנו בסלאק!</p>
+      <StackedResearchChart
+        graphData={graphData}
+        isLoading={loadingGraph}
+        field="total_actual_rides"
+        title="מספר נסיעות בפועל"
+        description="כמה נסיעות נרשמו כהתבצעו בכל יום/שעה בטווח הזמן שבחרתם. (נסיעות = siri rides)"
+      />
+      <StackedResearchChart
+        graphData={graphData}
+        isLoading={loadingGraph}
+        field="total_planned_rides"
+        title="מספר נסיעות מתוכננות"
+        description="כמה נסיעות היו אמורות להיות בכל יום/שעה בטווח הזמן שבחרתם. (נסיעות = נסיעות מתוכננות בgtfs)"
+      />
+      <StackedResearchChart
+        graphData={graphData}
+        isLoading={loadingGraph}
+        field="total_missed_rides"
+        title="מספר נסיעות שלא התבצעו"
+        description="כמה נסיעות היו אמורות להיות בכל יום/שעה בטווח הזמן שבחרתם אבל לא התבצעו. (הפרש בין שני הגרפים הקודמים)"
+      />
+    </Widget>
+  )
+}
+
+function StackedResearchInputs({
+  startDate,
+  setStartDate,
+  endDate,
+  setEndDate,
+  groupByHour,
+  setGroupByHour,
+}: {
+  startDate: moment.Moment
+  setStartDate: (date: moment.Moment) => void
+  endDate: moment.Moment
+  setEndDate: (date: moment.Moment) => void
+  groupByHour: boolean
+  setGroupByHour: (value: boolean) => void
+}) {
+  const { t } = useTranslation()
+  return (
+    <>
       <Grid container>
         <Grid xs={6} item>
           <DateSelector
@@ -96,7 +123,75 @@ export const StackedResearchChart = () => {
         />
         {t('group_by_hour_tooltip_content')}
       </label>
-      {loadingGraph ? (
+    </>
+  )
+}
+
+const StackedResearchChart = ({
+  graphData,
+  isLoading,
+  title,
+  description,
+  field = 'total_actual_rides',
+}: {
+  graphData: {
+    gtfs_route_date: string
+    gtfs_route_hour: string
+    operator_ref?: {
+      agency_name?: string
+    }
+    total_actual_rides: number
+    total_planned_rides: number
+  }[]
+  isLoading?: boolean
+  title?: string
+  description?: string
+  field?: 'total_actual_rides' | 'total_planned_rides' | 'total_missed_rides'
+}) => {
+  const data = useMemo(
+    () =>
+      graphData
+        .reduce((acc, curr) => {
+          const val =
+            field === 'total_missed_rides'
+              ? curr.total_planned_rides - curr.total_actual_rides
+              : curr[field]
+          const date = curr.gtfs_route_date ?? curr.gtfs_route_hour
+          const entry = acc.find((item) => item.date === date)
+          if (entry) {
+            if (val) entry[curr.operator_ref?.agency_name || 'Unknown'] = val
+          } else {
+            const newEntry = {
+              date: date,
+              [curr.operator_ref?.agency_name || 'Unknown']: val,
+            }
+            acc.push(newEntry)
+          }
+          return acc
+        }, [] as Record<string, string | number>[])
+        .sort((a, b) => {
+          if (a.date > b.date) return 1
+          if (a.date < b.date) return -1
+          return 0
+        }),
+    [graphData],
+  )
+
+  const operators = graphData
+    .map((operator) => operator.operator_ref?.agency_name || 'Unknown')
+    .filter(unique)
+
+  return (
+    <>
+      {title && <h3>{title}</h3>}
+      {description && (
+        <p>
+          <strong>מה רואים בגרף?</strong>
+          <br />
+          {description}
+        </p>
+      )}
+      {isLoading ? (
         <Skeleton active />
       ) : (
         <ResponsiveContainer width="100%" height="100%" minHeight="500px">
@@ -127,6 +222,6 @@ export const StackedResearchChart = () => {
           </AreaChart>
         </ResponsiveContainer>
       )}
-    </Widget>
+    </>
   )
 }
