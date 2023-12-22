c3c3315c3777267c4ccfa3017fe4fc663afa821a
moshe vilner <shootermv@gmail.com>
2023-12-22 12:51:24 +0200

    feat: add support for dark & light themes

--- src/layout/header/Header.tsx
+++ src/layout/header/Header.tsx
@@ -1,15 +1,30 @@
 import { Layout } from 'antd'
 import { MenuOutlined } from '@ant-design/icons'
 import { useContext } from 'react'
-import { LayoutContextInterface, LayoutCtx } from 'src/layout/LayoutContext'
-
+import { LayoutContextInterface, LayoutCtx } from '../LayoutContext'
+import { useTheme } from '../ThemeContext'
+import { BulbFilled, BulbOutlined } from '@ant-design/icons'
 const { Header } = Layout
 
 const MainHeader = () => {
   const { setDrawerOpen } = useContext<LayoutContextInterface>(LayoutCtx)
+  const { isDarkTheme, toggleTheme } = useTheme()
   return (
-    <Header style={{ background: 'white' }} className="hideOnDesktop">
-      <MenuOutlined onClick={() => setDrawerOpen(true)} />
+    <Header
+      style={{
+        background: isDarkTheme ? '#141414' : '#fff',
+        display: 'flex',
+        justifyContent: 'flex-end',
+        alignItems: 'center',
+      }}
+      className="_hideOnDesktop">
+      <MenuOutlined onClick={() => setDrawerOpen(true)} className="hideOnDesktop" />
+      <div style={{ flex: 1 }}>&nbsp;</div>
+      <button
+        onClick={toggleTheme}
+        style={{ border: 'none', background: 'transparent', cursor: 'pointer' }}>
+        {isDarkTheme ? <BulbOutlined style={{ color: '#fff' }} /> : <BulbFilled />}
+      </button>
     </Header>
   )
 }
