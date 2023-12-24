f231962ec5557c10479acc0f79aca4e8536275d4
Moshe <shootermv@gmail.com>
2023-12-24 19:45:50 +0200

    refactor: move styles to Header.css file
    Co-authored-by: NoamGaash <noam.gaash@gmail.com>

--- src/layout/header/Header.tsx
+++ src/layout/header/Header.tsx
@@ -4,20 +4,15 @@ import { useContext } from 'react'
 import { LayoutContextInterface, LayoutCtx } from '../LayoutContext'
 import { useTheme } from '../ThemeContext'
 import { BulbFilled, BulbOutlined } from '@ant-design/icons'
+import './Header.css'
+import cn from 'classnames'
 const { Header } = Layout
 
 const MainHeader = () => {
   const { setDrawerOpen } = useContext<LayoutContextInterface>(LayoutCtx)
   const { isDarkTheme, toggleTheme } = useTheme()
   return (
-    <Header
-      style={{
-        background: isDarkTheme ? '#141414' : '#fff',
-        display: 'flex',
-        justifyContent: 'flex-end',
-        alignItems: 'center',
-      }}
-      className="_hideOnDesktop">
+    <Header className={cn('main-header', { dark: isDarkTheme })}>
       <MenuOutlined onClick={() => setDrawerOpen(true)} className="hideOnDesktop" />
       <div style={{ flex: 1 }}>&nbsp;</div>
       <button
