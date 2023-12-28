1ab0129bb75fd4effd531c1699142a37d721e641
roni750 <yerushalmironi@gmail.com>
2023-12-28 20:00:04 +0200

    Implement useCallback with a state-toggle function

--- src/pages/realtimeMap/index.tsx
+++ src/pages/realtimeMap/index.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useMemo, useState } from 'react'
+import { useCallback, useEffect, useMemo, useState } from 'react'
 import { MapContainer, Marker, Polyline, Popup, TileLayer, useMap } from 'react-leaflet'
 import MarkerClusterGroup from 'react-leaflet-cluster'
 import { useTranslation } from 'react-i18next'
@@ -43,6 +43,8 @@ const fourMinutesAgo = moment(fiveMinutesAgo).add(1, 'minutes')
 
 export default function RealtimeMapPage() {
   const [isExpanded, setIsExpanded] = useState<boolean>(false)
+  const toggleExpanded = useCallback(() => setIsExpanded((expanded) => !expanded), [])
+
   const position: Point = {
     loc: [32.3057988, 34.85478613], // arbitrary default value... Netanya - best city to live & die in
     color: 0,
