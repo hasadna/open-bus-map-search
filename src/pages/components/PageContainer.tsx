723156445654781d3f67f1563f996257f1f2f7b8
Shon Aizenshtein <68730456+Senior-ai@users.noreply.github.com>
2024-01-03 20:12:50 +0200

    fixed scrolling bug

--- src/pages/components/PageContainer.tsx
+++ src/pages/components/PageContainer.tsx
@@ -2,7 +2,6 @@ import styled from 'styled-components'
 import { MARGIN_MEDIUM } from '../../resources/sizes'
 
 export const PageContainer = styled.div`
-  overflow-x: hidden;
   display: flex;
   flex-direction: column;
   gap: ${MARGIN_MEDIUM}px;
