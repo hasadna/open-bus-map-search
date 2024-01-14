d5c6b235c9492295325d9a208a0ea5d00fc30168
i5x64BIT <vaintomer@gmail.com>
2024-01-14 09:55:29 +0200

    Format dates to ArriveByTimeChart
    - set default settings for moment.js in App.tsx (tz and locale)
    - display day + date on by-day
    - display day + hour on by-hour

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,11 @@ import router from './routes'
 import { RouterProvider } from 'react-router-dom'
 import { Suspense } from 'react'
 import Preloader from './shared/Preloader'
+import moment from 'moment-timezone'
+import "moment/dist/locale/he"
+
+moment.tz.setDefault('Asia/Jerusalem');
+moment.locale("he");
 
 export const RoutedApp = () => (
   <Suspense fallback={<Preloader />}>
