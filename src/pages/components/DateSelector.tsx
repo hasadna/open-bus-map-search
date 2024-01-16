2374fa4e38b4f93ffceeb5aede59fc482753cf6d
Ori Broda <ori.broda@mail.huji.ac.il>
2024-01-16 19:09:10 +0200

    feat: add informative warning for invalid date format

--- src/pages/components/DateSelector.tsx
+++ src/pages/components/DateSelector.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useState, useMemo } from 'react'
 import { DatePicker } from '@mui/x-date-pickers/DatePicker'
 import { useTranslation } from 'react-i18next'
 import { DataAndTimeSelectorProps } from './utils/dateAndTime'
@@ -13,6 +13,20 @@ export function DateSelector({ time, onChange, customLabel }: DataAndTimeSelecto
   const [error, setError] = useState<DateValidationError | null>(null)
   const { t } = useTranslation()
 
+  const errorMessage = useMemo(() => {
+    switch (error) {
+      case 'maxDate':
+      case 'minDate':
+      case 'invalidDate': {
+        return t('bug_date_invalid_format')
+      }
+
+      default: {
+        return ''
+      }
+    }
+  }, [error])
+
   return (
     <>
       <DatePicker
@@ -23,6 +37,11 @@ export function DateSelector({ time, onChange, customLabel }: DataAndTimeSelecto
         label={customLabel || t('choose_date')}
         disableFuture
         onError={(err) => setError(err)}
+        slotProps={{
+          textField: {
+            helperText: errorMessage,
+          },
+        }}
       />
       {error && <Error>{error}</Error>}
     </>
