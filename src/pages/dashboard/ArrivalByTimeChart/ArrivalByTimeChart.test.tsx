dd9237191a69ea686172fe1b3b9e346a9d3a0154
R894 <kamil.khalaileh@gmail.com>
2024-01-30 02:54:00 +0200

    test: add ArrivalByTimeChart test

---
+++ src/pages/dashboard/ArrivalByTimeChart/ArrivalByTimeChart.test.tsx
@@ -0,0 +1,74 @@
+import { render, screen } from '@testing-library/react'
+import ArrivalByTimeChart from './ArrivalByTimeChart'
+import '@testing-library/jest-dom'
+import { ReactNode } from 'react'
+import testBusData from './testdata/data.json'
+
+describe('ArrivalByTimeChart', () => {
+  interface Data {
+    id: string
+    name: string
+    current: number
+    max: number
+    percent: number
+    gtfs_route_date: string
+    gtfs_route_hour: string
+  }
+
+  let testData: Data[]
+
+  beforeAll(() => {
+    testData = testBusData
+  })
+
+  // Mock ResponsiveContainer to have a static size otherwise it doesn't render in the unit test
+  vi.mock('recharts', () => {
+    // eslint-disable-next-line @typescript-eslint/no-var-requires
+    const Recharts = require('recharts')
+
+    return {
+      ...Recharts,
+      ResponsiveContainer: ({ children }: { children: ReactNode }) => (
+        <Recharts.ResponsiveContainer width={800} height={800}>
+          {children}
+        </Recharts.ResponsiveContainer>
+      ),
+    }
+  })
+
+  test('matches snapshot', () => {
+    const { asFragment } = render(
+      <ArrivalByTimeChart data={testData} operatorId={testData[0].id} />,
+    )
+
+    expect(asFragment()).toMatchSnapshot()
+  })
+
+  test('renders without crashing', () => {
+    render(<ArrivalByTimeChart data={testData} operatorId={testData[0].id} />)
+
+    expect(screen.getByText(testData[0].name)).toBeInTheDocument()
+  })
+
+  test('tooltip wrapper exists', () => {
+    render(<ArrivalByTimeChart data={testData} operatorId={testData[0].id} />)
+    expect(document.querySelector('.recharts-tooltip-wrapper')).not.toBeNull
+  })
+
+  test('tooltips are not visible by default', () => {
+    render(<ArrivalByTimeChart data={testData} operatorId={testData[0].id} />)
+    expect(document.querySelector('.recharts-tooltip-wrapper')).not.toBeVisible
+  })
+
+  test('legend wrapper exists', () => {
+    render(<ArrivalByTimeChart data={testData} operatorId={testData[0].id} />)
+    expect(document.querySelector('.recharts-legend-wrapper')).not.toBeVisible
+  })
+
+  test('filters operators correctly', () => {
+    render(<ArrivalByTimeChart data={testData} operatorId={testData[0].id} />)
+
+    expect(screen.queryByText('notegged')).not.toBeInTheDocument()
+    expect(screen.queryByText('Unknown')).not.toBeInTheDocument()
+  })
+})
