c150ad2a34bb940421a67aba479da5974f11a01e
Idan Dover <idan.dava@gmail.com>
2024-02-08 16:40:49 +0200

    fix: fixed linting problem

--- src/layout/ThemeContext.tsx
+++ src/layout/ThemeContext.tsx
@@ -1,47 +1,47 @@
-import { FC, PropsWithChildren, createContext, useContext } from "react";
+import { FC, PropsWithChildren, createContext, useContext } from 'react';
 import {
   ThemeProvider as MuiThemeProvider,
   createTheme,
-} from "@mui/material/styles";
-import ScopedCssBaseline from "@mui/material/ScopedCssBaseline";
-import { ConfigProvider, theme } from "antd";
-import heIL from "antd/es/locale/he_IL";
-import { useTranslation } from "react-i18next";
-import { useLocalStorage } from "src/locale/useLocalStorage";
+} from '@mui/material/styles';
+import ScopedCssBaseline from '@mui/material/ScopedCssBaseline';
+import { ConfigProvider, theme } from 'antd';
+import heIL from 'antd/es/locale/he_IL';
+import { useTranslation } from 'react-i18next';
+import { useLocalStorage } from 'src/locale/useLocalStorage';
 
 export interface ThemeContextInterface {
-  toggleTheme: () => void;
-  isDarkTheme?: boolean;
+  toggleTheme: () => void
+  isDarkTheme?: boolean
 }
 
-const ThemeContext = createContext({} as ThemeContextInterface);
+const ThemeContext = createContext({} as ThemeContextInterface)
 
 const darkTheme = createTheme({
   palette: {
-    mode: "dark",
+    mode: 'dark',
   },
-});
+})
 
 const lightTheme = createTheme({
   palette: {
-    mode: "light",
+    mode: 'light',
   },
-});
+})
 
-const { defaultAlgorithm, darkAlgorithm, compactAlgorithm } = theme;
+const { defaultAlgorithm, darkAlgorithm } = theme
 
 export const ThemeProvider: FC<PropsWithChildren> = ({ children }) => {
-  const [isDarkTheme, setIsDarkTheme] = useLocalStorage<boolean>("isDarkTheme");
+  const [isDarkTheme, setIsDarkTheme] = useLocalStorage<boolean>('isDarkTheme');
 
-  const { i18n } = useTranslation();
+  const { i18n } = useTranslation()
   const toggleTheme = () => {
-    setIsDarkTheme((prevTheme: boolean) => !prevTheme);
-  };
+    setIsDarkTheme((prevTheme: boolean) => !prevTheme)
+  }
 
   const contextValue = {
     isDarkTheme,
     toggleTheme,
-  };
+  }
 
   return (
     <ConfigProvider
@@ -50,22 +50,21 @@ export const ThemeProvider: FC<PropsWithChildren> = ({ children }) => {
       theme={{
         algorithm: isDarkTheme ? darkAlgorithm : defaultAlgorithm,
         token: {
-          colorBgBase: isDarkTheme ? "#1c1d1c" : "#ffffff",
+          colorBgBase: isDarkTheme ? '#1c1d1c' : '#ffffff',
         },
       }}
     >
       <MuiThemeProvider theme={isDarkTheme ? darkTheme : lightTheme}>
         <ScopedCssBaseline enableColorScheme>
           <ThemeContext.Provider value={contextValue}>
-            {" "}
-            {children}{" "}
+            {children}
           </ThemeContext.Provider>
         </ScopedCssBaseline>
       </MuiThemeProvider>
     </ConfigProvider>
-  );
-};
+  )
+}
 
 export const useTheme = () => {
-  return useContext(ThemeContext);
-};
+  return useContext(ThemeContext)
+}
