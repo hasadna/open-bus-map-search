d160336dd6bff5a238623aa7679710cad5018559
R894 <kamil.khalaileh@gmail.com>
2024-02-14 01:04:44 +0200

    remove extra beforeEach in setupTests

--- setupTests.ts
+++ setupTests.ts
@@ -1,12 +1,8 @@
 import '@testing-library/jest-dom'
 import { cleanup } from '@testing-library/react'
-import { getPastDate, setBrowserTime, test } from 'tests/utils'
 // runs a cleanup after each test case (e.g. clearing jsdom)
 
 global.ResizeObserver = require('resize-observer-polyfill')
-test.beforeEach(async ({ page }) => {
-  await setBrowserTime(getPastDate(), page)
-})
 
 afterEach(() => {
   cleanup()
