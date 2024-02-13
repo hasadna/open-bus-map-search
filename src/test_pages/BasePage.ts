25428e804f9deb22c4188226fd9f406d342e6ae6
R894 <kamil.khalaileh@gmail.com>
2024-02-13 02:56:28 +0200

    add setFakeTime to BasePage.ts

--- src/test_pages/BasePage.ts
+++ src/test_pages/BasePage.ts
@@ -52,4 +52,26 @@ export abstract class BasePage {
     })
     return options
   }
+
+  async setFakeTime(date: Date) {
+    const fakeNow = date.valueOf()
+
+    // Update the Date accordingly
+    await this.page.addInitScript(`{
+      // Extend Date constructor to default to fakeNow
+      Date = class extends Date {
+        constructor(...args) {
+          if (args.length === 0) {
+            super(${fakeNow});
+          } else {
+            super(...args);
+          }
+        }
+      }
+      // Override Date.now() to start from fakeNow
+      const __DateNowOffset = ${fakeNow} - Date.now();
+      const __DateNow = Date.now;
+      Date.now = () => __DateNow() + __DateNowOffset;
+    }`)
+  }
 }
