346a107feb596af797835f3735bb97bfbc7aa18f
R894 <kamil.khalaileh@gmail.com>
2024-02-14 01:07:02 +0200

    modify setupTests.ts

--- setupTests.ts
+++ setupTests.ts
@@ -3,7 +3,6 @@ import { cleanup } from '@testing-library/react'
 // runs a cleanup after each test case (e.g. clearing jsdom)
 
 global.ResizeObserver = require('resize-observer-polyfill')
-
 afterEach(() => {
   cleanup()
 })
