9505f28772c67ca5da348b3f17807b951adfee59
Tomer Vainshtein <vaintomer@gmail.com>
2024-02-25 11:16:52 +0200

    set rule to error

--- eslint.config.js
+++ eslint.config.js
@@ -59,7 +59,7 @@ module.exports = [
       '@typescript-eslint/no-base-to-string': 'off',
       '@typescript-eslint/no-unsafe-assignment': 'off',
       '@typescript-eslint/no-unsafe-argument': 'off',
-      "import/no-unused-modules": [1, {"unusedExports": true}],
+      "import/no-unused-modules": "error" ,
       'prettier/prettier': [
         'error',
         {
