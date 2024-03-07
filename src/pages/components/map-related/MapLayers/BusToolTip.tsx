bc58970c1a1a1ed140ae4c78c2fe70f182bb18e2
Noam Gaash <noam.gaash@applitools.com>
2024-03-07 19:18:15 +0200

    Revert "feat: Added Treeview feature (#558)"
    This reverts commit 4aa6213ae1824a931666e7a6335fbd32aa1673f9.

--- src/pages/components/map-related/MapLayers/BusToolTip.tsx
+++ src/pages/components/map-related/MapLayers/BusToolTip.tsx
@@ -10,7 +10,6 @@ import { SiriRideWithRelatedPydanticModel } from 'open-bus-stride-client/openapi
 import { useTranslation } from 'react-i18next'
 import { Spin } from 'antd'
 import cn from 'classnames'
-import CustomTreeView from '../../CustomTreeView'
 
 export type BusToolTipProps = { position: Point; icon: string }
 
@@ -95,16 +94,11 @@ export function BusToolTip({ position, icon }: BusToolTipProps) {
         {showJson ? t('hide_document') : t('show_document')}
       </Button>
       {showJson && (
-        <div onClick={(e) => e.stopPropagation()}>
-          <CustomTreeView<Point> id={position.point?.id + ''} data={position} name={t('line')} />
-          {siriRide?.gtfsRideId && (
-            <CustomTreeView<SiriRideWithRelatedPydanticModel | undefined>
-              id={siriRide?.gtfsRideId + ''}
-              data={siriRide}
-              name={t('drive_direction')}
-            />
-          )}
-        </div>
+        <pre>
+          {JSON.stringify(position, null, 2)}
+          <br />
+          {siriRide && JSON.stringify(siriRide, null, 2)}
+        </pre>
       )}
     </div>
   )
