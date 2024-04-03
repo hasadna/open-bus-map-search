3f495afbcec6c43a681584e6dacf73d346408e88
Kiril <vtkkllx2367@gmail.com>
2024-04-03 21:03:37 +0300

    feat: 'InfoYoutubeModal' add iframe

--- src/pages/components/YoutubeModal.tsx
+++ src/pages/components/YoutubeModal.tsx
@@ -1,8 +1,9 @@
 import { useState } from 'react'
 import { InfoCircleOutlined } from '@ant-design/icons'
 import { createPortal } from 'react-dom'
+import { Modal } from 'antd'
 
-const InfoYoutubeModal = ({ videoUrl }: { videoUrl: string }) => {
+const InfoYoutubeModal = ({ videoUrl, title }: { videoUrl: string; title: 'string' }) => {
   const [visible, setVisible] = useState(false)
 
   return (
@@ -12,7 +13,29 @@ const InfoYoutubeModal = ({ videoUrl }: { videoUrl: string }) => {
         style={{ marginRight: '12px' }}
         aria-label="youtube-video-about"
       />
-      {visible && createPortal(<p>HELLO</p>, document.body)}
+      <Modal
+        width={'1000px'}
+        footer={null}
+        title={title}
+        open={visible}
+        onCancel={() => setVisible(false)}>
+        <div
+          style={{
+            height: '0',
+            paddingBottom: '52.4%',
+            width: '100%',
+            position: 'relative',
+          }}>
+          <iframe
+            frameBorder={0}
+            style={{ position: 'absolute', width: '100%', height: '100%' }}
+            allowFullScreen
+            width="932px"
+            height="524px"
+            src={videoUrl}
+          />
+        </div>
+      </Modal>
     </>
   )
 }
