3eb61b072db859e9d6ef9754f8b334041aa56bcb
israel zusha rotenberg <100128949+zushar@users.noreply.github.com>
2024-04-23 12:05:12 +0300

    reafctor: Update language toggle button to use useTranslation fonction for aria-labal and title and chaing the translatio

--- src/layout/header/LanguageToggle.tsx
+++ src/layout/header/LanguageToggle.tsx
@@ -3,7 +3,7 @@ import { useTranslation } from 'react-i18next'
 import { GlobalOutlined } from '@ant-design/icons'
 
 export const LanguageToggle = () => {
-  const { i18n } = useTranslation()
+  const { t, i18n } = useTranslation()
   const [, handleChangeLanguage] = useReducer((state: string) => {
     const newLanguage = { he: 'en', en: 'he' }[state]
     i18n.changeLanguage(newLanguage)
@@ -14,8 +14,8 @@ export const LanguageToggle = () => {
     <button
       className="theme-icon"
       onClick={handleChangeLanguage}
-      aria-label={i18n.language === 'he' ? 'English' : 'עברית'}
-      title={i18n.language === 'he' ? 'English' : 'עברית'}
+      aria-label={t('Change Language')}
+      title={t('Change Language')}
       style={{ margin: '1em', border: 'none', background: 'transparent', cursor: 'pointer' }}>
       <GlobalOutlined style={{ fontSize: '1.7em' }} />
     </button>
