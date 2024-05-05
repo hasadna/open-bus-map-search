c80851f04ab1f12a84976c0745411971a6039beb
R894 <54629307+R894@users.noreply.github.com>
2024-05-05 17:21:27 +0300

    feat: add caching to groupByService (#727)

--- src/index.tsx
+++ src/index.tsx
@@ -4,12 +4,16 @@ import './index.css'
 import App from './App'
 import ReactGA from 'react-ga4'
 import './locale/allTranslations'
+import { QueryClient, QueryClientProvider } from 'react-query'
 
 ReactGA.initialize('G-0YRQT80GG1')
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
+const client = new QueryClient()
 root.render(
   <React.StrictMode>
-    <App />
+    <QueryClientProvider client={client}>
+      <App />
+    </QueryClientProvider>
   </React.StrictMode>,
 )
