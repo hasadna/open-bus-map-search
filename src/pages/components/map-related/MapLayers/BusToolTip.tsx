bb4c4f148f4c359337371d516236ea9794c6d8c6
Ori Hazan <orihsecond@gmail.com>
2024-05-09 12:20:52 +0300

    feat(bus-tooltip): add collapse section for the extra data

--- src/pages/components/map-related/MapLayers/BusToolTip.tsx
+++ src/pages/components/map-related/MapLayers/BusToolTip.tsx
@@ -1,16 +1,16 @@
 import { useEffect, useState } from 'react'
 import { Link } from 'react-router-dom'
 import { Point } from 'src/pages/timeBasedMap'
-import { Button } from '@mui/material'
+import { Button, Collapse } from '@mui/material'
 import moment from 'moment-timezone'
 import './BusToolTip.scss'
-
 import { getSiriRideWithRelated } from 'src/api/siriService'
 import { SiriRideWithRelatedPydanticModel } from 'open-bus-stride-client/openapi/models/SiriRideWithRelatedPydanticModel'
 import { useTranslation } from 'react-i18next'
 import { Spin } from 'antd'
 import cn from 'classnames'
 import CustomTreeView from '../../CustomTreeView'
+import { ExpandMore } from '../../ExpandMore'
 
 export type BusToolTipProps = { position: Point; icon: string }
 
@@ -18,6 +18,7 @@ export function BusToolTip({ position, icon }: BusToolTipProps) {
   const [siriRide, setSiriRide] = useState<SiriRideWithRelatedPydanticModel | undefined>()
   const [isLoading, setIsLoading] = useState(false)
   const [showJson, setShowJson] = useState(false)
+  const [expanded, setExpanded] = useState(false)
   const { t } = useTranslation()
 
   useEffect(() => {
@@ -34,15 +35,15 @@ export function BusToolTip({ position, icon }: BusToolTipProps) {
   return (
     <div className={cn({ 'extend-for-json': showJson }, 'bus-tooltip')}>
       {isLoading || !siriRide ? (
-        <>
-          {t('loading_routes')}
+        <div className="loading">
+          <span>{t('loading_routes')}</span>
           <Spin />
-        </>
+        </div>
       ) : (
         <>
           <header className="header">
             <h1 className="title">
-              {t('line')} :
+              {`${t('line')}: `}
               <span>
                 <Link to={`/profile/${siriRide.gtfsRideGtfsRouteId}`}>
                   {siriRide.gtfsRouteRouteShortName}
@@ -53,17 +54,20 @@ export function BusToolTip({ position, icon }: BusToolTipProps) {
           </header>
           <ul>
             <li>
-              {t('from')} :<span>{siriRide.gtfsRouteRouteLongName?.split('<->')[0]}</span>
+              {`${t('from')}: `}
+              <span>{siriRide.gtfsRouteRouteLongName?.split('<->')[0]}</span>
             </li>
             <li>
-              {t('destination')} :<span>{siriRide.gtfsRouteRouteLongName?.split('<->')[1]}</span>
+              {`${t('destination')}: `}
+              <span>{siriRide.gtfsRouteRouteLongName?.split('<->')[1]}</span>
             </li>
             <li>
-              {t('velocity')} :<span>{`${position.point?.velocity}  ${t('kmh')}`}</span>
+              {`${t('velocity')}: `}
+              <span>{`${position.point?.velocity}  ${t('kmh')}`}</span>
             </li>
 
             <li>
-              {t('sample_time')} :
+              {`${t('sample_time')}: `}
               <span>
                 {/* eslint-disable-next-line @typescript-eslint/no-unnecessary-type-assertion */}
                 {moment(position.point!.recorded_at_time as string, moment.ISO_8601)
@@ -72,39 +76,50 @@ export function BusToolTip({ position, icon }: BusToolTipProps) {
               </span>
             </li>
             <li>
-              {t('vehicle_ref')} :<span>{position.point?.siri_ride__vehicle_ref}</span>
+              {`${t('vehicle_ref')}: `}
+              <span>{position.point?.siri_ride__vehicle_ref}</span>
             </li>
           </ul>
-          {/*maybe option to add info like this in extend card for now I  put this condition */}
-          {window.screen.height > 1100 && (
-            <>
-              <h4>
-                {t('drive_direction')} :
+          <ExpandMore
+            expand={expanded}
+            onClick={() => setExpanded(!expanded)}
+            aria-expanded={expanded}
+            aria-label="show more"
+          />
+          <Collapse in={expanded} timeout="auto" unmountOnExit>
+            <ul className="extra-data">
+              <li>
+                {`${t('drive_direction')}: `}
                 <span>
-                  ( {position.point?.bearing} {t('bearing')})
+                  ({position.point?.bearing} {t('bearing')})
                 </span>
-              </h4>
-              <h4>
-                {t('coords')} :<span>{position.loc.join(' , ')}</span>
-              </h4>
-            </>
-          )}
+              </li>
+              <li>
+                {`${t('coords')}: `}
+                <span>{position.loc.join(' ,')}</span>
+              </li>
+            </ul>
+            <Button sx={{ paddingLeft: 0 }} onClick={() => setShowJson((showJson) => !showJson)}>
+              {showJson ? t('hide_document') : t('show_document')}
+            </Button>
+            {showJson && (
+              <div onClick={(e) => e.stopPropagation()}>
+                <CustomTreeView<Point>
+                  id={position.point?.id + ''}
+                  data={position}
+                  name={t('line')}
+                />
+                {siriRide?.gtfsRideId && (
+                  <CustomTreeView<SiriRideWithRelatedPydanticModel | undefined>
+                    id={siriRide?.gtfsRideId + ''}
+                    data={siriRide}
+                    name={t('drive_direction')}
+                  />
+                )}
+              </div>
+            )}
+          </Collapse>
         </>
-      )}
-      <Button onClick={() => setShowJson((showJson) => !showJson)}>
-        {showJson ? t('hide_document') : t('show_document')}
-      </Button>
-      {showJson && (
-        <div onClick={(e) => e.stopPropagation()}>
-          <CustomTreeView<Point> id={position.point?.id + ''} data={position} name={t('line')} />
-          {siriRide?.gtfsRideId && (
-            <CustomTreeView<SiriRideWithRelatedPydanticModel | undefined>
-              id={siriRide?.gtfsRideId + ''}
-              data={siriRide}
-              name={t('drive_direction')}
-            />
-          )}
-        </div>
       )}
     </div>
   )
