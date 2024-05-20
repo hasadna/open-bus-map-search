d7de5723a96c4ab4b44047ab5bb8f78e7a7985cb
menachem95 <menachemf95@gmail.com>
2024-05-20 14:12:22 +0300

    Change dependency on useEffect in useRecenterOnDataChange hook

--- src/pages/components/map-related/useRecenterOnDataChange.ts
+++ src/pages/components/map-related/useRecenterOnDataChange.ts
@@ -3,7 +3,7 @@ import { LatLngTuple } from 'leaflet'
 import { useMap } from 'react-leaflet'
 import { MapProps } from './map-types'
 
-export function useRecenterOnDataChange({ positions, plannedRouteStops }: MapProps) {
+export function useRecenterOnDataChange({ positions }: MapProps) {
   const map = useMap()
   const positionsSum = positions.reduce(
     (acc, { loc }) => [acc[0] + loc[0], acc[1] + loc[1]],
@@ -15,5 +15,5 @@ export function useRecenterOnDataChange({ positions, plannedRouteStops }: MapPro
     if (mean[0] || mean[1]) {
       map.setView(mean, map.getZoom(), { animate: true })
     }
-  }, [positions, plannedRouteStops])
+  }, [mean])
 }
