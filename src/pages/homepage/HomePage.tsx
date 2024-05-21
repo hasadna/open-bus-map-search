dd5d841c283510419c3f02b964526c29996d0b93
Ori Hazan <orihsecond@gmail.com>
2024-05-21 22:32:17 +0300

    feat(homepage): implement homepage design according to figma

--- src/pages/homepage/HomePage.tsx
+++ src/pages/homepage/HomePage.tsx
@@ -1,34 +1,53 @@
-import { NavLink } from 'react-router-dom'
+import { NavLink, To } from 'react-router-dom'
 import busImage from '../../img/busImg.png'
 import './HomePage.scss'
+import { useTranslation } from 'react-i18next'
+import {
+  DirectionsBusOutlined,
+  HistoryOutlined,
+  MapOutlined,
+  ViewKanbanOutlined,
+} from '@mui/icons-material'
+import { SvgIconProps } from '@mui/material'
 
 export const HomePage = () => {
+  const { t } = useTranslation()
+
   return (
-    <>
+    <div className="container">
       <img src={busImage} alt="Public Transportaion Bus Illustration" />
-      <h1>ברוכים הבאים לדאטאבוס</h1>
-      <h2>הפלטפורמה הפתוחה לנתוני אמת על איכות קווי התחבורה הציבורית בישראל </h2>
-      <h3>
-        מטרת האתר היא לשפר את איכות התחבורה הציבורית בארץ ע״י מתן מידע אמין לעיתונאים, אזחרים, חברות
-        התחבורה, וגורמי ממשל בישראל.
-      </h3>
-      <div className="main-menu">
-        <NavLink to="/timeline" className="nav-link">
-          הסטוריית לוחות זמנים
-        </NavLink>
-        <NavLink to="/gaps" className="nav-link">
-          {' '}
-          נסיעות שלא בוצעו{' '}
-        </NavLink>
-        <NavLink to="/gaps_pattern" className="nav-link">
-          דפוסי נסיעות שלא בוצעו
-        </NavLink>
-        <NavLink to="/map" className="nav-link">
-          מפה לפי זמן
-        </NavLink>
-      </div>
-    </>
+      <h1>{t('homepage.welcome')}</h1>
+      <h2>{t('homepage.databus_definition')}</h2>
+      <p>{t('homepage.website_goal')}</p>
+      <section className="links">
+        <PageLink icon={<HistoryOutlined />} label={t('homepage.timeline')} to="/timeline" />
+        <PageLink icon={<DirectionsBusOutlined />} label={t('homepage.gaps')} to="/gaps" />
+        <PageLink
+          icon={<ViewKanbanOutlined />}
+          label={t('homepage.gaps_patterns')}
+          to="/gaps_patterns"
+        />
+        <PageLink icon={<MapOutlined />} label={t('homepage.map')} to="/map" />
+      </section>
+      <footer>{`${t('homepage.copyright')} ${new Date().getFullYear()}`}</footer>
+    </div>
   )
 }
+
+const PageLink = ({
+  icon,
+  label,
+  to,
+}: {
+  icon: React.ReactElement<SvgIconProps>
+  label: string
+  to: To
+}) => (
+  <div className="page-link">
+    {icon}
+    <span>{label}</span>
+    <NavLink to={to}>הצג</NavLink>
+  </div>
+)
 
 export default HomePage
