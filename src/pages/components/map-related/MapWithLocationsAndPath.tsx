a360e02b231c03fed0fe8dd858c80204d4aca19d
menachem95 <menachemf95@gmail.com>
2024-05-28 10:27:08 +0300

    .

--- src/pages/components/map-related/MapWithLocationsAndPath.tsx
+++ src/pages/components/map-related/MapWithLocationsAndPath.tsx
@@ -1,24 +1,18 @@
 import { MapContainer } from 'react-leaflet'
 import { Point } from 'src/pages/timeBasedMap'
-import { MapContent } from './MapContent'
 import { MapProps } from './map-types'
 import { useCallback, useState } from 'react'
 import { Button } from 'antd'
 import { ExpandAltOutlined } from '@ant-design/icons'
 import '../../Map.scss'
-import { useTheme } from 'src/layout/ThemeContext'
-import cn from 'classnames'
+import { MapContent } from './MapContent'
 
 const position: Point = {
   loc: [32.3057988, 34.85478613], // arbitrary default value... Netanya - best city to live & die in
   color: 0,
 }
 
-
 export function MapWithLocationsAndPath({ positions, plannedRouteStops }: MapProps) {
-
-  const { isDarkTheme } = useTheme()
-
   const [isExpanded, setIsExpanded] = useState<boolean>(false)
   const toggleExpanded = useCallback(() => setIsExpanded((expanded) => !expanded), [])
 
@@ -32,6 +26,7 @@ export function MapWithLocationsAndPath({ positions, plannedRouteStops }: MapPro
         icon={<ExpandAltOutlined />}
       />
       <MapContainer center={position.loc} zoom={13} scrollWheelZoom={true}>
+        <MapContent positions={positions} plannedRouteStops={plannedRouteStops} />
       </MapContainer>
     </div>
   )
