2eeaa18d58a17f27bea55c298e074ed1d48f0d48
Dorin zrihen <zrihend@gmail.com>
2024-06-02 15:09:07 +0300

    feat: Test | header-links icons should be unique

--- tests/mainHeader.spec.ts
+++ tests/mainHeader.spec.ts
@@ -1,16 +1,15 @@
 import { test, expect } from './utils'
 
-test.beforeEach(async ({ page}) => {
+test.beforeEach(async ({ page }) => {
   await page.goto('/')
 })
 
 test("the main header doesn't show duplicate icons", async ({ page }) => {
   const headerLocator = page.locator('div.header-links')
   const svgLocators = headerLocator.locator('svg')
-  
   const svgCount = await svgLocators.count()
   const svgInnerHTML = []
-  for(let i = 0; i < svgCount; i++) {
+  for(let i = 0 ; i < svgCount ; i++) {
     const innerHTML = await svgLocators.nth(i).innerHTML()
     svgInnerHTML.push(innerHTML)
   }
