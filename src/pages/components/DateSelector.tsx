3e889834f7fcbfa76a7d3e433bf17ac6b6a6ac24
YUVIYUV <yuval.masada@gmail.com>
2024-06-20 17:37:12 +0300

    fix: replace useMemo with regular function

--- src/pages/components/DateSelector.tsx
+++ src/pages/components/DateSelector.tsx
@@ -1,27 +1,33 @@
-import { useState, useMemo } from 'react'
+import { useState } from 'react'
 import { DatePicker } from '@mui/x-date-pickers/DatePicker'
-import { useTranslation } from 'react-i18next'
+import { t } from 'i18next'
 import { DateValidationError } from '@mui/x-date-pickers'
 import { DataAndTimeSelectorProps } from './utils/dateAndTime'
 
+type errorMessageTypes = '' | 'bug_date_alert' | 'bug_date_invalid_format'
+const getErrorMessage = (error: DateValidationError): errorMessageTypes => {
+  let message: errorMessageTypes = ''
+  switch (error) {
+    case 'maxDate':
+    case 'minDate':
+      message = t('bug_date_alert')
+      break
+    case 'invalidDate': {
+      message = t('bug_date_invalid_format')
+      break
+    }
+
+    default: {
+      message = ''
+    }
+  }
+  return message
+}
+
 export function DateSelector({ time, onChange, customLabel, minDate }: DataAndTimeSelectorProps) {
   const [error, setError] = useState<DateValidationError | null>(null)
-  const { t } = useTranslation()
 
-  const errorMessage = useMemo(() => {
-    switch (error) {
-      case 'maxDate':
-      case 'minDate':
-        return t('bug_date_alert')
-      case 'invalidDate': {
-        return t('bug_date_invalid_format')
-      }
-
-      default: {
-        return ''
-      }
-    }
-  }, [error, t])
+  const errorMessage = getErrorMessage(error)
 
   return (
     <DatePicker
