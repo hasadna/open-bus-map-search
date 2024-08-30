31543d7b7a4e52949e656435cec7b9033de3f389
Tom Rytt <tomofekrytt@gmail.com>
2024-08-30 12:25:55 +0300

    fix- improved card viability on dark mode

--- src/shared/Widget.tsx
+++ src/shared/Widget.tsx
@@ -1,9 +1,13 @@
 import Card from '@mui/material/Card'
 import CardContent from '@mui/material/CardContent'
+import cn from 'classnames'
+
+import { useTheme } from 'src/layout/ThemeContext'
 
 const Widget = (props: { children: React.ReactNode }) => {
+  const { isDarkTheme } = useTheme()
   return (
-    <Card>
+    <Card className={cn('card', { dark: isDarkTheme })}>
       <CardContent>{props.children} </CardContent>
     </Card>
   )
