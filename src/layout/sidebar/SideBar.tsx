90ae0becf4f1b2d78518123a1aea5bb2826ace75
Tzivya Fireman <tz0534199716@gmail.com>
2024-09-29 20:02:47 +0300

    fix: Fixed sidebar collapse arrow direction for RTL languages (#889)
    Co-authored-by: Noam Gaash <noam.gaash@gmail.com>

--- src/layout/sidebar/SideBar.tsx
+++ src/layout/sidebar/SideBar.tsx
@@ -2,6 +2,7 @@ import './sidebar.scss'
 import { Drawer, Layout } from 'antd'
 import { useContext, useState } from 'react'
 import { Link } from 'react-router-dom'
+import { useTranslation } from 'react-i18next'
 import { LayoutContextInterface, LayoutCtx } from '../LayoutContext'
 import Menu from './menu/Menu'
 import { Logo } from './logo'
@@ -11,8 +12,12 @@ const { Sider } = Layout
 const CollapsedLogo = () => <h1 className={'sidebar-logo-collapsed'}>🚌</h1>
 
 export default function SideBar() {
+  const { i18n } = useTranslation()
   const { drawerOpen, setDrawerOpen } = useContext<LayoutContextInterface>(LayoutCtx)
   const [collapsed, setCollapsed] = useState(false)
+
+  const isRtl = i18n.language === 'he'
+
   return (
     <>
       <Drawer
@@ -35,6 +40,7 @@ export default function SideBar() {
         width={250}
         collapsible
         collapsed={collapsed}
+        reverseArrow={isRtl}
         onCollapse={(value: boolean) => setCollapsed(value)}
         className="hideOnMobile">
         <Link to={PAGES[0].path} replace>
