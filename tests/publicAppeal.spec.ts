5ea05c4fd4c79718b8d593f5763626deef497254
Yael Chen <51058584+YaelChen@users.noreply.github.com>
2024-12-03 14:38:37 +0200

    test: public appeal page items (#958)

--- tests/publicAppeal.spec.ts
+++ tests/publicAppeal.spec.ts
@@ -6,6 +6,18 @@ test('"קול קורא" menu item should redirect to the public appeal page', as
   await expect(page).toHaveURL(/public-appeal/)
 })
 
+test('Public Appeal page items', async ({ page }) => {
+  await page.goto('/')
+  await page.getByRole('link', { name: 'קול קורא' }).click()
+  const itemsInOrder = [
+    'איפה נדרשים נתיבי תעדוף לתחבורה ציבורית (נת"צים)?',
+    'חישוב שעת היציאה מתחנת המוצא, ושעת ההגעה לתחנות השונות במסלול',
+    'איך לשייך קווי אוטובוס לאזורים גיאוגרפיים?',
+    'התקבצות אוטובוסים',
+  ]
+  await expect(page.locator('h2')).toContainText(itemsInOrder)
+})
+
 test('clicking "Open Bus Stride API" link should lead to "https://open-bus-stride-api.hasadna.org.il/docs"', async ({
   page,
 }) => {
