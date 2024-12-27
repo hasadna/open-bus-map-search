2777a75295a9e61a0c84266f5785f8e13fba3d2e
Tom Rytt <tomofekrytt@gmail.com>
2024-12-27 16:55:21 +0200

    changed name to LanguageToggleButton to better reflect what it is since actual toggle logic moved to context

--- src/layout/header/LanguageToggleButton.tsx
+++ src/layout/header/LanguageToggleButton.tsx
@@ -2,7 +2,7 @@ import { useTranslation } from 'react-i18next'
 import { GlobalOutlined } from '@ant-design/icons'
 import { useTheme } from '../ThemeContext'
 
-export const LanguageToggleBtn = () => {
+export const LanguageToggleButton = () => {
   const { toggleLanguage } = useTheme()
   const { t } = useTranslation()
 
