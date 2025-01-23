40ac63b6a8b2b976633d7e27ff1ab03bf2fe50f5
Yael Chen <51058584+YaelChen@users.noreply.github.com>
2025-01-23 09:23:48 +0200

    Update bugReport.spec.ts
    remove comments

--- tests/bugReport.spec.ts
+++ tests/bugReport.spec.ts
@@ -7,25 +7,7 @@ test('the YouTube modal in "report a bug" is visible and have the correct src', 
   await page.getByLabel('bug').locator('svg').click()
   await page.getByLabel('לפתוח סרטון על העמוד הזה').locator('svg').click()
   const iframeElement = await page.waitForSelector('iframe')
-
-  // const Visible = await iframeElement.isVisible()
-  // expect(Visible).toBe(true)
-
-  // A specific element is visible.
-  // await expect(page.getBy
-  //await expect(page.getByTitle('YouTube video player')).toBeVisible();
   expect(iframeElement.isVisible())
-
-  // // At least one item in the list is visible.
-  // //await expect(page.getByTestId('todo-item').first()).toBeVisible();
-
-  // // At least one of the two elements is visible, possibly both.
-  // await expect(
-  //     page.getByRole('button', { name: 'Sign in' })
-  //         .or(page.getByRole('button', { name: 'Sign up' }))
-  //         .first()
-  // ).toBeVisible();
-
   const videoSrc = page.locator('iframe')
   await expect(videoSrc).toHaveAttribute(
     'src',
