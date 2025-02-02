e70189663c51943caa5f2501f832a7b7f66d7585
YaelChen <ibexgrace@gmail.com>
2025-02-02 21:03:02 +0200

    test: add test for a missing request type in the form

--- tests/bugReport.spec.ts
+++ tests/bugReport.spec.ts
@@ -1,11 +1,41 @@
-import { test } from '@playwright/test'
+import { test, expect } from '@playwright/test'
+
+test.beforeEach(async ({ page }) => {
+  await page.goto('/')
+})
 
 test('An instruction video for Report a bug', async ({ page }) => {
-  await page.goto('/')
   await page.getByLabel('bug').locator('svg').click()
   await page.getByLabel('לפתוח סרטון על העמוד הזה').locator('svg').click()
   await page.locator('iframe').contentFrame().getByLabel('Play', { exact: true }).click()
   await page.locator('iframe').contentFrame().getByLabel('Pause keyboard shortcut k').click()
   await page.getByLabel('Close', { exact: true }).click()
   await page.getByLabel('לפתוח סרטון על העמוד הזה').press('ControlOrMeta+c')
+})
+
+test('bug missing field - request type', async ({ page }) => {
+  await test.step('Open bug report modal', async () => {
+    await page.getByLabel('bug').locator('svg').click()
+  })
+
+  await test.step('Fill required fields', async () => {
+    await page.getByLabel('כותרת/סיכום').fill('Test')
+    await page.getByLabel('שם מלא').fill('Nils Holgerson')
+    await page.getByLabel('אי-מייל').fill('Muli@gmail.com')
+    await page.getByLabel('תיאור').fill('חסרות הנסיעות של המפקדת אקה')
+    await page.getByLabel('סביבה (דפדפן, מערכת)').fill('כרום')
+    await page.getByLabel('התנהגות צפויה').fill('אקה נודדת לארצות הקור')
+    await page.getByLabel('התנהגות נוכחית').fill('לא קיימת')
+    await page.getByLabel('באיזו תדירות זה קורה?').fill('כל קיץ')
+  })
+
+  await test.step('Submit the form', async () => {
+    await page.getByRole('button', { name: 'שלח את הדוח' }).click()
+  })
+
+  await test.step('Verify missing field error', async () => {
+    const errorMessage = page.locator('.ant-form-item-explain-error')
+    await expect(errorMessage).toBeVisible()
+    await expect(errorMessage).toHaveText('אנא הזן סוג בקשה!')
+  })
 })
