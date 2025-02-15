58f2f9e62a6193174ea8a170fc1f22592e92b1b5
Noam Gaash <noam.gaash@applitools.com>
2025-02-15 23:00:02 +0200

    feat(about): add version info component

---
+++ src/pages/about/version/VersionInfo.tsx
@@ -0,0 +1,28 @@
+import { useTranslation } from 'react-i18next'
+import { useQuery } from '@tanstack/react-query'
+import Widget from 'src/shared/Widget'
+
+import './VersionInfo.scss'
+
+const versionUrl = 'https://open-bus-map-search.hasadna.org.il/hash.txt'
+
+export const VersionInfo = () => {
+  const { t } = useTranslation()
+  // const [version, setVersion] = useState<string | null>(null)
+  const {
+    data: version,
+    isLoading,
+    isError,
+  } = useQuery({
+    queryKey: ['version'],
+    queryFn: () => fetch(versionUrl).then((response) => response.text()),
+  })
+  return (
+    <Widget>
+      <h2>{t('version')}</h2>
+      {isLoading && <p>{t('loading')}</p>}
+      {isError && <p>{t('failedToFetchVersion')}</p>}
+      {version && <p>{version}</p>}
+    </Widget>
+  )
+}
