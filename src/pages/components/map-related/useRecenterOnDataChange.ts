e2c03d804d8aac9412d27787b15a4da685ea9f1f
aviv <avivabachi@gmail.com>
2025-03-30 21:25:27 +0300

    refactor: improve logic in useRecenterOnDataChange

--- src/pages/components/map-related/useRecenterOnDataChange.ts
+++ src/pages/components/map-related/useRecenterOnDataChange.ts
@@ -2,31 +2,45 @@ import { useEffect } from 'react'
 import { LatLngTuple } from 'leaflet'
 import { useMap } from 'react-leaflet'
 import { MapProps } from './map-types'
+import { BusStop } from 'src/model/busStop'
+import { Point } from 'src/pages/timeBasedMap'
+
+const calculatePoint = (acc: LatLngTuple, curr: Point | BusStop): LatLngTuple => {
+  if ('loc' in curr && curr.loc) {
+    return [acc[0] + curr.loc[0], acc[1] + curr.loc[1]]
+  }
+  if ('location' in curr && curr.location) {
+    return [acc[0] + curr.location.latitude, acc[1] + curr.location.longitude]
+  }
+  return acc
+}
+
+const calculateMean = (
+  positionsSum: LatLngTuple,
+  stopsSum: LatLngTuple,
+  totalPoints: number,
+): LatLngTuple => {
+  return [
+    (positionsSum[0] + stopsSum[0]) / totalPoints,
+    (positionsSum[1] + stopsSum[1]) / totalPoints,
+  ]
+}
 
 export function useRecenterOnDataChange({ positions = [], plannedRouteStops = [] }: MapProps) {
   const map = useMap()
 
   useEffect(() => {
-    if (!positions.length && !plannedRouteStops.length) return
+    if (positions.length === 0 && plannedRouteStops.length === 0) return
 
-    const positionsSum = positions.reduce(
-      ([lat, lng], pos) => [lat + pos.loc[0], lng + pos.loc[1]],
-      [0, 0],
-    )
-    const stopsSum = plannedRouteStops.reduce(
-      ([lat, lng], stop) => [lat + stop.location.latitude, lng + stop.location.longitude],
-      [0, 0],
-    )
-
+    const positionsSum = positions.reduce(calculatePoint, [0, 0])
+    const stopsSum = plannedRouteStops.reduce(calculatePoint, [0, 0])
     const totalPoints = positions.length + plannedRouteStops.length
 
-    const mean: LatLngTuple = [
-      (positionsSum[0] + stopsSum[0]) / totalPoints,
-      (positionsSum[1] + stopsSum[1]) / totalPoints,
-    ]
+    if (totalPoints === 0) return
 
-    // console.log('mean: ', mean)
-    if (mean[0] || mean[1]) {
+    const mean = calculateMean(positionsSum, stopsSum, totalPoints)
+
+    if (mean[0] !== 0 || mean[1] !== 0) {
       map.setView(mean, map.getZoom(), { animate: true })
     }
   }, [positions, plannedRouteStops, map])
