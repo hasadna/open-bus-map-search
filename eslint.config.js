ad33631aaaad3718f070a948570391086114f918
aviv <avivabachi@gmail.com>
2025-04-04 21:58:25 +0300

    refactor: update eslint configuration to use js module and import.meta

--- eslint.config.js
+++ eslint.config.js
@@ -1,17 +1,17 @@
 import nxPlugin from '@nx/eslint-plugin'
 import { FlatCompat } from '@eslint/eslintrc'
-import eslintPluginReact, { configs } from 'eslint-plugin-react'
+import eslintPluginReact from 'eslint-plugin-react'
 import eslintPluginReactHooks from 'eslint-plugin-react-hooks'
 import eslintPluginImport from 'eslint-plugin-import'
-import typescriptEslintEslintPlugin, { configs as _configs } from '@typescript-eslint/eslint-plugin'
+import typescriptEslintEslintPlugin from '@typescript-eslint/eslint-plugin'
 import typescriptEslintParser from '@typescript-eslint/parser'
-import { browser, es2021 } from 'globals'
-import { configs as __configs } from '@eslint/js'
+import globals from 'globals'
+import eslintJs from '@eslint/js'
 import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended'
 
 const compat = new FlatCompat({
-  baseDirectory: __dirname,
-  recommendedConfig: __configs.recommended,
+  baseDirectory: import.meta.dirname,
+  recommendedConfig: eslintJs.configs.recommended,
 })
 
 export default [
@@ -36,28 +36,24 @@ export default [
         sourceType: 'module',
         project: './tsconfig.json',
       },
-      globals: { ...browser, ...es2021 },
+      globals: { ...globals.browser, ...globals.es2021 },
     },
   },
   {
     rules: {
-      ..._configs.recommended.rules,
-      ...configs.recommended.rules,
+      ...typescriptEslintEslintPlugin.configs.recommended.rules,
+      ...typescriptEslintEslintPlugin.configs['recommended-requiring-type-checking'].rules,
+      ...eslintPluginReact.configs.recommended.rules,
       'react-hooks/rules-of-hooks': 'error',
       'react/jsx-filename-extension': [1, { extensions: ['.tsx'] }],
-      ..._configs['eslint-recommended'].rules,
-      ..._configs['recommended'].rules,
-      ..._configs['recommended-requiring-type-checking'].rules,
-      ...configs['recommended'].rules,
       'react/react-in-jsx-scope': 'off',
       '@typescript-eslint/no-explicit-any': 'warn',
-      // ...eslintPluginImport.configs.recommended.rules,
       '@typescript-eslint/no-unsafe-call': 'off',
       '@typescript-eslint/no-unsafe-member-access': 'off',
       '@typescript-eslint/no-floating-promises': 'off',
       '@typescript-eslint/restrict-template-expressions': 'off',
       '@typescript-eslint/no-base-to-string': 'off',
-      '@typescript-eslint/no-unsafe-assignment': 'off',
+      '@typescript-eslint.no-unsafe-assignment': 'off',
       'import/no-unused-modules': 'error',
       'import/order': 'error',
       'prettier/prettier': [
