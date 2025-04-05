58ed9124ad8afb6396705bc408153da1caf24282
aviv <avivabachi@gmail.com>
2025-04-05 18:28:38 +0300

    fix: correct import.meta usage and improve sitemap generation logic

--- sitemap.js
+++ sitemap.js
@@ -1,8 +1,11 @@
 import { readFileSync, writeFileSync } from 'fs'
-import { join } from 'path'
+import { join, dirname } from 'path'
+import { fileURLToPath } from 'url'
 
 const sitemap = () => {
-  const app = readFileSync(join(import.meta.dirname, '/src/routes/index.tsx'), 'utf8')
+  const __dirname = dirname(fileURLToPath(import.meta.url))
+  const app = readFileSync(join(__dirname, '/src/routes/index.tsx'), 'utf8')
+
   const routes = app.match(/'\/[a-z_-]*'/g)
   const urls = routes.map((route) => {
     const url = route.slice(1, -1)
@@ -10,21 +13,21 @@ const sitemap = () => {
   })
 
   const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
-    <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-        ${urls
-          .map(
-            (url) => `
+  <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+  ${urls
+    .map(
+      (url) => `
             <url>
-                <loc>https://open-bus-map-search.hasadna.org.il${url}</loc>
-                <changefreq>weekly</changefreq>
-                <priority>0.8</priority>
-                <lastmod>${new Date().toISOString()}</lastmod>
-             </url> 
-          `,
-          )
-          .join('\n')}
-    </urlset>`
-  writeFileSync(join(import.meta.dirname, '/public/sitemap.xml'), sitemap)
+            <loc>https://open-bus-map-search.hasadna.org.il${url}</loc>
+            <changefreq>weekly</changefreq>
+            <priority>0.8</priority>
+            <lastmod>${new Date().toISOString()}</lastmod>
+            </url> 
+            `,
+    )
+    .join('\n')}
+          </urlset>`
+  writeFileSync(join(__dirname, '/public/sitemap.xml'), sitemap)
 }
 
 sitemap()
