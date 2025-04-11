42d9eddf44cd89936e3f41ec7ffa674d49ae3a77
aviv <avivabachi@gmail.com>
2025-04-11 16:46:18 +0300

    fix: process.env

--- src/api/apiConfig.ts
+++ src/api/apiConfig.ts
@@ -2,8 +2,6 @@ import { Configuration } from 'open-bus-stride-client'
 
 //const BASE_PATH = 'http://localhost:3000/api'
 export const BASE_PATH = process.env.VITE_BASE_PATH
-console.log(BASE_PATH)
-
 export const API_CONFIG = new Configuration({ basePath: BASE_PATH })
 
 export const MAX_HITS_COUNT = 16
--- vite.config.ts
+++ vite.config.ts
@@ -1,40 +1,46 @@
+import { loadEnv } from 'vite'
 import { defineConfig } from 'vitest/config'
 import react from '@vitejs/plugin-react-swc'
 import IstanbulPlugin from 'vite-plugin-istanbul'
 
-const ASSET_URL = process.env.ASSET_URL || ''
-
 // https://vitejs.dev/config/
-export default defineConfig({
-  base: ASSET_URL,
-  plugins: [
-    react(),
-    ...(process.env.USE_BABEL_PLUGIN_ISTANBUL
-      ? [
-          IstanbulPlugin({
-            include: 'src/*',
-            exclude: ['node_modules', 'test/'],
-            extension: ['.js', '.ts', '.tsx'],
-          }),
-        ]
-      : []),
-  ],
-  resolve: {
-    alias: {
-      src: '/src',
-    },
-  },
-  server: {
-    port: 3000,
-  },
-  test: {
-    environment: 'jsdom',
-    globals: true,
-    setupFiles: './setupTests.ts',
-    exclude: [
-      '**/node_modules/**',
-      '**/dist/**',
-      '**/*.spec.*' /* do not include playwright files */,
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd())
+
+  return {
+    base: env?.ASSET_URL || '',
+    plugins: [
+      react(),
+      ...(env?.USE_BABEL_PLUGIN_ISTANBUL
+        ? [
+            IstanbulPlugin({
+              include: 'src/*',
+              exclude: ['node_modules', 'test/'],
+              extension: ['.js', '.ts', '.tsx'],
+            }),
+          ]
+        : []),
     ],
-  },
+    resolve: {
+      alias: {
+        src: '/src',
+      },
+    },
+    define: {
+      'process.env': env,
+    },
+    server: {
+      port: 3000,
+    },
+    test: {
+      environment: 'jsdom',
+      globals: true,
+      setupFiles: './setupTests.ts',
+      exclude: [
+        '**/node_modules/**',
+        '**/dist/**',
+        '**/*.spec.*' /* do not include playwright files */,
+      ],
+    },
+  }
 })
