aa86f13ba2fead4ba8138b398f8f5ff958249a42
aviv <avivabachi@gmail.com>
2025-04-17 12:59:07 +0300

    refactor: replace TreeView with SimpleTreeView for improved performance and update renderTree function

--- src/pages/components/CustomTreeView.tsx
+++ src/pages/components/CustomTreeView.tsx
@@ -1,4 +1,4 @@
-import { TreeView } from '@mui/x-tree-view/TreeView'
+import { SimpleTreeView } from '@mui/x-tree-view/SimpleTreeView'
 import { TreeItem } from '@mui/x-tree-view/TreeItem'
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore'
 import ChevronRightIcon from '@mui/icons-material/ChevronRight'
@@ -68,7 +68,7 @@ const objectToTreeNode = <T extends Record<string, unknown>>(
 
 // Render tree function utilizing the TreeNode interface
 const renderTree = <T extends BaseTreeNode>(nodes: TreeNode<T>): JSX.Element => (
-  <TreeItem key={nodes.id} nodeId={nodes.id} label={nodes.name}>
+  <TreeItem key={nodes.id} itemId={nodes.id} label={nodes.name}>
     {nodes.children?.map((node) => renderTree(node))}
   </TreeItem>
 )
@@ -77,12 +77,14 @@ const renderTree = <T extends BaseTreeNode>(nodes: TreeNode<T>): JSX.Element => 
 const CustomTreeView = <T,>({ data, name, id }: CustomTreeViewProps<T>) => {
   const dataAsTreeNode = objectToTreeNode({ id, name, children: [data] })
   return (
-    <TreeView
-      defaultCollapseIcon={<ExpandMoreIcon />}
-      defaultExpandIcon={<ChevronRightIcon />}
-      defaultEndIcon={<div style={{ width: 24 }} />}>
+    <SimpleTreeView
+      slots={{
+        collapseIcon: ExpandMoreIcon,
+        expandIcon: ChevronRightIcon,
+        endIcon: () => <div style={{ width: 24 }} />,
+      }}>
       {renderTree(dataAsTreeNode)}
-    </TreeView>
+    </SimpleTreeView>
   )
 }
 
