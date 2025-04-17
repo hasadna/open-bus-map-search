8cb43aee46862f2aed51df86c98a87b1df7b1a14
aviv <avivabachi@gmail.com>
2025-04-17 21:14:59 +0300

    fix: reorder import statements in YoutubeModal stories file

--- src/pages/components/YoutubeModal.stories.tsx
+++ src/pages/components/YoutubeModal.stories.tsx
@@ -1,6 +1,6 @@
 import type { Meta, StoryObj } from '@storybook/react'
-import InfoYoutubeModal from './YoutubeModal'
 import { useTranslation } from 'react-i18next'
+import InfoYoutubeModal from './YoutubeModal'
 
 const meta: Meta<typeof InfoYoutubeModal> = {
   title: 'Components/InfoYoutubeModal',
