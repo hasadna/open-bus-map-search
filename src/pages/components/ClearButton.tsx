0d2285fc9c8d1c462b13284aa1b940ca0978f254
aviv <avivabachi@gmail.com>
2025-04-18 16:18:38 +0300

    refactor: ClearButton component by removing unused

--- src/pages/components/ClearButton.tsx
+++ src/pages/components/ClearButton.tsx
@@ -1,13 +1,8 @@
 import { CloseOutlined as ClearIcon } from '@ant-design/icons'
 import './ClearButton.scss'
 
-interface ClearButtonProps {
-  onClearInput: () => void
-  disabled?: boolean
-}
-
-const ClearButton = ({ onClearInput, disabled }: ClearButtonProps) => {
-  return <ClearIcon onClick={onClearInput} disabled={disabled} className="clear-indicator" />
+const ClearButton = ({ onClearInput }: { onClearInput: () => void }) => {
+  return <ClearIcon onClick={onClearInput} className="clear-indicator" />
 }
 
 export default ClearButton
