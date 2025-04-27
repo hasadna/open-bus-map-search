127cddce148d7dd05a20301549571423c7a33556
aviv <avivabachi@gmail.com>
2025-04-27 19:59:38 +0300

    refactor: improve

--- src/pages/operator/OperatorGaps.tsx
+++ src/pages/operator/OperatorGaps.tsx
@@ -2,6 +2,7 @@ import { useMemo } from 'react'
 import { Stack, Typography } from '@mui/material'
 import { PieChart, Pie, Cell } from 'recharts'
 import { useTranslation } from 'react-i18next'
+import { Skeleton } from 'antd'
 import moment from 'moment'
 import { InfoItem, InfoTable } from '../components/InfoTable'
 import Widget from 'src/shared/Widget'
@@ -15,7 +16,7 @@ export const OperatorGaps = ({
   timestamp?: number
 }) => {
   const { t, i18n } = useTranslation()
-  const [groupByOperatorData] = useGroupBy({
+  const [groupByOperatorData, isLoading] = useGroupBy({
     dateFrom: moment(timestamp).add(-1, 'day').endOf('day'),
     dateTo: moment(timestamp).endOf('day'),
     groupBy: 'operator_ref',
@@ -26,46 +27,55 @@ export const OperatorGaps = ({
 
     return [
       { name: t('ride_as_planned'), value: operator?.total_planned_rides, color: '#00C49F' },
-      { name: t('ride_missing'), value: operator?.total_routes, color: '#FF4040' },
       { name: t('ride_extra'), value: 2000, color: '#FFBB28' },
+      { name: t('ride_missing'), value: operator?.total_routes, color: '#FF4040' },
     ]
   }, [operatorId, timestamp, groupByOperatorData, i18n.language])
 
   return (
     <Widget>
-      <Stack flexDirection="row" justifyContent="space-between">
-        <div>
-          <Typography variant="h5">Daily Statistics</Typography>
-          <InfoTable>
-            {data.map((d) => (
-              <InfoItem key={d.name} lable={d.name} value={d.value} />
-            ))}
-          </InfoTable>
-        </div>
-        <PieChart width={160} height={160}>
-          <Pie data={data} innerRadius={60} outerRadius={80} paddingAngle={5} dataKey="value">
-            {data.map((entry) => (
-              <Cell key={entry.name} fill={entry.color} />
-            ))}
-          </Pie>
-          <text
-            x="50%"
-            y="50%"
-            textAnchor="middle"
-            dominantBaseline="middle"
-            fill="black"
-            fontSize="32"
-            fontWeight={500}>
-            {data[0].value && data[1].value && data[2].value
-              ? (
-                  ((data[0].value + data[2].value) /
-                    (data[2].value + data[1].value + data[0].value)) *
-                  100
-                ).toFixed(1) + '%'
-              : ''}
-          </text>
-        </PieChart>
-      </Stack>
+      <Typography variant="h5">{t('operator.statistics')}</Typography>
+      {isLoading ? (
+        <Skeleton active paragraph={{ rows: 4 }} />
+      ) : (
+        <Stack flexDirection="row" justifyContent="space-between">
+          <div>
+            <InfoTable>
+              {data.map((d) => (
+                <InfoItem key={d.name} lable={d.name} value={d.value} />
+              ))}
+            </InfoTable>
+          </div>
+          <PieChart width={160} height={160}>
+            <Pie data={data} innerRadius={65} outerRadius={80} paddingAngle={5} dataKey="value">
+              {data.map((entry) => (
+                <Cell key={entry.name} fill={entry.color} />
+              ))}
+            </Pie>
+            <text
+              x="50%"
+              y="50%"
+              textAnchor="middle"
+              dominantBaseline="middle"
+              fill="currentColor"
+              fontSize="32"
+              fontWeight={500}>
+              {calculatePercentage(data[0].value, data[2].value, data[1].value)}
+            </text>
+          </PieChart>
+        </Stack>
+      )}
     </Widget>
   )
+}
+
+const calculatePercentage = (
+  planned: number | undefined = 0,
+  missing: number | undefined = 0,
+  extra: number | undefined = 0,
+) => {
+  const total = planned + missing + extra
+  if (total === 0) return ''
+  const percentage = ((planned + extra) / total) * 100
+  return `${percentage.toFixed(1)}%`
 }
--- src/pages/operator/OperatorInfo.tsx
+++ src/pages/operator/OperatorInfo.tsx
@@ -12,37 +12,32 @@ export const OperatorInfo = ({ operatorId }: { operatorId?: string }) => {
 
   return (
     <Widget>
+      <Typography variant="h5">
+        {i18n.language === 'en' ? operator?.eng_name : operator?.name}
+      </Typography>
       <Stack justifyContent="space-between" flexDirection="row">
-        <div>
-          <Typography variant="h5">
-            {i18n.language === 'he' ? operator?.name : operator?.eng_name}
-          </Typography>
-          <InfoTable>
-            <InfoItem lable={t('operator.ref')} value={operator?.ref} />
-            {i18n.language === 'en' && (
-              <InfoItem lable={'Hebrew Name'} value={`"${operator?.name}"`} />
-            )}
-            <InfoItem lable={t('operator.founded')} value={operator?.founded} />
-            <InfoItem
-              lable={t('operator.area_title')}
-              value={operator?.area && t(`operator.area.${operator?.area}`)}
-            />
-            <InfoItem
-              lable={t('operator.type_title')}
-              value={operator?.type && t(`operator.type.${operator?.type}`)}
-            />
-            <InfoItem
-              lable={t('operator.website')}
-              value={
-                operator?.website && (
-                  <a dir="ltr" href={operator?.website} target="_blank" rel="noreferrer">
-                    {operator?.website}
-                  </a>
-                )
-              }
-            />
-          </InfoTable>
-        </div>
+        <InfoTable>
+          <InfoItem lable={t('operator.ref')} value={operator?.ref} />
+          <InfoItem lable={t('operator.founded')} value={operator?.founded} />
+          <InfoItem
+            lable={t('operator.area_title')}
+            value={operator?.area && t(`operator.area.${operator?.area}`)}
+          />
+          <InfoItem
+            lable={t('operator.type_title')}
+            value={operator?.type && t(`operator.type.${operator?.type}`)}
+          />
+          <InfoItem
+            lable={t('operator.website')}
+            value={
+              operator?.website && (
+                <a dir="ltr" href={operator?.website} target="_blank" rel="noreferrer">
+                  {operator?.website}
+                </a>
+              )
+            }
+          />
+        </InfoTable>
         <img src={`../operators-logos/${operator?.ref}.svg`} height={96} />
       </Stack>
     </Widget>
