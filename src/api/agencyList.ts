737c29c49a7b3bdb678144dd6865206d954f31e4
aviv_abachi <avivabachi@gmail.com>
2025-05-21 11:57:12 +0300

    refactor: getAgencyList

--- src/api/agencyList.ts
+++ src/api/agencyList.ts
@@ -21,21 +21,21 @@ const tryDates = [
  * @returns Agency data array, might contain DUPLICATE agencies with different `date` values
  */
 export default async function getAgencyList(): Promise<Agency[]> {
-  if (agencyList) return agencyList
-
-  let data: Agency[] = []
-  for (const date of tryDates) {
-    try {
-      const response = await fetch(`${BASE_PATH}/gtfs_agencies/list?date_from=${date}`)
-      if (!response.ok) continue
-      data = (await response.json()) as Agency[]
-      if (data.length > 0) break
-    } catch (err) {
-      console.error(err)
+  if (!agencyList) {
+    let data: Agency[] = []
+    for (const date of tryDates) {
+      try {
+        const response = await fetch(`${BASE_PATH}/gtfs_agencies/list?date_from=${date}`)
+        if (!response.ok) continue
+        data = (await response.json()) as Agency[]
+        if (data.length > 0) break
+      } catch (err) {
+        console.error(err)
+      }
     }
+    agencyList = data.filter(Boolean)
   }
 
-  agencyList = data.filter(Boolean)
   return agencyList
 }
 
