6f7753c73df6affc9e0c35c5ea68816f36f2ac3d
aviv_abachi <avivabachi@gmail.com>
2025-05-23 15:56:14 +0300

    refactor: Enhance useLocalStorage hook to support default values and improve state initialization

--- src/locale/useLocalStorage.ts
+++ src/locale/useLocalStorage.ts
@@ -1,10 +1,15 @@
 import { useState } from 'react'
 
-export function useLocalStorage<T>(key: string) {
+export function useLocalStorage<T>(key: string, defaultValue?: T) {
   try {
-    const [state, setState] = useState(JSON.parse(localStorage.getItem(key) ?? 'null') as T)
+    const [state, setState] = useState<T | undefined>(() => {
+      const item = localStorage.getItem(key)
+      if (item !== null) return JSON.parse(item) as T
+      if (defaultValue !== undefined) localStorage.setItem(key, JSON.stringify(defaultValue))
+      return defaultValue
+    })
 
-    const setItem = (value: T | ((val: T) => T)) => {
+    const setItem = (value: T | ((val: T | undefined) => T)) => {
       const valueToStore = value instanceof Function ? value(state) : value
       setState(valueToStore)
       localStorage.setItem(key, JSON.stringify(valueToStore))
@@ -14,7 +19,7 @@ export function useLocalStorage<T>(key: string) {
   } catch (error) {
     console.log('No support for local storage', error)
     return [
-      undefined,
+      defaultValue,
       () => {
         throw new Error('No support for local storage')
       },
