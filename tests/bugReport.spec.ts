38e69caa6999e7f4b30d37983831b02763a7a22f
Aviv <AvivAbachi@gmail.com>
2025-06-09 20:48:55 +0300

    test: Refactor Operators Test and Bug Report Test Improvements (#1136)

--- tests/bugReport.spec.ts
+++ tests/bugReport.spec.ts
@@ -15,7 +15,6 @@ test('An instruction video for Report a bug', async ({ page }) => {
     'https://www.youtube-nocookie.com/embed?v=F6sD9Bz4Xj0&list=PL6Rh06rT7uiX1AQE-lm55hy-seL3idx3T&index=11',
   )
   await page.locator('iframe').contentFrame().getByLabel('Play', { exact: true }).click()
-  await page.locator('iframe').contentFrame().getByLabel('Pause keyboard shortcut k').click()
   await page.getByLabel('Close', { exact: true }).click()
   await page.getByLabel('לפתוח סרטון על העמוד הזה').press('ControlOrMeta+c')
 })
