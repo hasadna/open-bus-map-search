3a6464531bddd03f0e21bd002145fa9da1c572f4
Aviv <AvivAbachi@Gmail.com>
2025-06-16 15:54:30 +0300

    chore: update Storybook dependencies and migrate imports to react-vite
    - Updated @applitools/eyes-storybook from ^3.55.5 to ^3.55.6
    - Updated Storybook dependencies from version 8.6.14 to 9.0.10
    - Changed import statements in all Storybook files from '@storybook/react' to '@storybook/react-vite'

--- src/pages/components/map-related/MapContent.stories.tsx
+++ src/pages/components/map-related/MapContent.stories.tsx
@@ -1,4 +1,4 @@
-import type { Meta, StoryObj } from '@storybook/react'
+import type { Meta, StoryObj } from '@storybook/react-vite'
 import { MapContainer } from 'react-leaflet'
 import { MapContent } from './MapContent'
 import { filteredPositions, plannedRouteStops } from './mapStorybookData'
