fbd86207579dadfa3d93d357e07edbf1dfc66942
aviv <avivabachi@gmail.com>
2025-06-18 19:42:58 +0300

    refactor: add wait time after skeletons are hidden in Applitools config

--- applitools.config.cjs
+++ applitools.config.cjs
@@ -5,6 +5,7 @@ module.exports = {
   waitBeforeCapture: async (page) => {
     try {
       await page.waitForSelector('.ant-skeleton', { state: 'hidden', timeout: 180000 })
+      await page.waitForTimeout(500)
     } catch (error) {
       console.warn('Error waiting for skeletons to disappear:', error.message)
     }
