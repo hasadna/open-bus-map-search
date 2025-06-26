69d3bfc0caf04b5047199a88902ae108c8bf0220
aviv <avivabachi@gmail.com>
2025-06-26 15:36:47 +0300

    fix: update Playwright installation to include Chromium and remove no-sandbox option

--- playwright.config.ts
+++ playwright.config.ts
@@ -18,10 +18,6 @@ export default defineConfig({
   timeout: 3 * 60 * 1000,
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
-    launchOptions: {
-      args: ['--no-sandbox'],
-    },
-
     /* Base URL to use in actions like `await page.goto('/')`. */
     baseURL: 'http://localhost:3000',
 
