cc8b65a0a669d0a26dd7690fd2456019a9c07e9f
Noam Gaash <noam.gaash@applitools.com>
2025-07-12 09:40:59 +0300

    refactor: enhance waitBeforeCapture logic and remove deprecated config file

--- applitools.config.cjs
+++ applitools.config.cjs
@@ -1,7 +1,6 @@
-/**
- * @type {import('@applitools/eyes-playwright').Configuration}
- */
 module.exports = {
+  testConcurrency:20,
+  
   waitBeforeCapture: async () => {
     const startTime = Date.now()
     while (
@@ -9,13 +8,15 @@ module.exports = {
       document.querySelector('.ant-skeleton')
     ) {
       await new Promise((resolve) => setTimeout(resolve, 100))
-      if (Date.now() - startTime > 10000) {
-        console.warn('Waited too long for skeletons to disappear, proceeding anyway.')
-        break
+      if (Date.now() - startTime > 50000) {
+        // debugger
+        console.warn('Waited too long for skeletons to disappear.')
+        throw new Error('Skeletons did not disappear in time on ' + window.location.href)
       }
     }
     return true
   },
   // puppeteerOptions: {headless: false, devtools: true},
   showBrowserLogs: true,
+  // showLogs: true,// uncomment to see Applitools logs
 }
