b5105c9790f30d599846001e7aadd53228c82426
aviv <avivabachi@gmail.com>
2025-07-19 11:08:03 +0300

    chore: update skeleton wait time in Applitools configuration
    - Increased the wait time for skeletons to disappear from 600000ms to 180000ms in the Applitools configuration to better handle longer loading scenarios during tests.

--- applitools.config.cjs
+++ applitools.config.cjs
@@ -3,12 +3,14 @@ module.exports = {
 
   waitBeforeCapture: async () => {
     const startTime = Date.now()
+    const timeout = 3 * 60 * 1000
     while (
       document.querySelector('.ant-skeleton-content') ||
       document.querySelector('.ant-skeleton')
     ) {
+      const duration = Date.now() - startTime
       await new Promise((resolve) => setTimeout(resolve, 100))
-      if (Date.now() - startTime > 600_000) {
+      if (duration > timeout) {
         console.warn('Waited too long for skeletons to disappear.')
         throw new Error('Skeletons did not disappear in time on ' + window.location.href)
       }
