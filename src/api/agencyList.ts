fd00981c5cc2de39fa811254effa0dd418462cd1
aviv <avivabachi@gmail.com>
2025-08-13 18:22:29 +0300

    refactor: optimze

--- src/api/agencyList.ts
+++ src/api/agencyList.ts
@@ -1,7 +1,7 @@
 import { GtfsAgencyPydanticModel } from '@hasadna/open-bus-api-client'
 import { GTFS_API } from './apiConfig'
 
-let agencyList: GtfsAgencyPydanticModel[]
+let agencyListPromise: Promise<GtfsAgencyPydanticModel[]> | null = null
 
 const tryDates = [
   new Date(Date.now() - 24 * 60 * 60 * 1000),
@@ -9,19 +9,22 @@ const tryDates = [
   new Date('2025-05-18'),
 ]
 
-export default async function getAgencyList(): Promise<GtfsAgencyPydanticModel[]> {
-  if (!agencyList) {
-    let data: GtfsAgencyPydanticModel[] = []
-    for (const date of tryDates) {
-      try {
-        data = await GTFS_API.gtfsAgenciesListGet({ dateFrom: date })
-        if (data.length > 0) break
-      } catch (err) {
-        console.error(err)
-      }
+async function fetchAgencyList() {
+  let data: GtfsAgencyPydanticModel[] = []
+  for (const date of tryDates) {
+    try {
+      data = await GTFS_API.gtfsAgenciesListGet({ dateFrom: date })
+      if (data.length > 0) break
+    } catch (err) {
+      console.error('Error fetching agencies:', err)
     }
-    agencyList = data.filter(Boolean)
   }
+  return data.filter(Boolean)
+}
 
-  return agencyList
+export function getAgencyList() {
+  if (!agencyListPromise) {
+    agencyListPromise = fetchAgencyList()
+  }
+  return agencyListPromise
 }
--- src/hooks/useAgencyList.ts
+++ src/hooks/useAgencyList.ts
@@ -1,6 +1,6 @@
 import { GtfsAgencyPydanticModel } from '@hasadna/open-bus-api-client'
 import { useState, useEffect } from 'react'
-import getAgencyList from 'src/api/agencyList'
+import { getAgencyList } from 'src/api/agencyList'
 
 export function useAgencyList() {
   const [agencyList, setAgencyList] = useState<GtfsAgencyPydanticModel[]>([])
