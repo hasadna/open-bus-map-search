747ebd7b57b87742e097a3d691e9db924da0f585
Aviv <AvivAbachi@gmail.com>
2025-08-14 16:38:37 +0300

    refactor: agency list (#1236)

--- src/api/agencyList.ts
+++ src/api/agencyList.ts
@@ -1,50 +1,30 @@
-import { useEffect, useState } from 'react'
-import { STRIDE_API_BASE_PATH } from './apiConfig'
-import dayjs from 'src/dayjs'
+import { GtfsAgencyPydanticModel } from '@hasadna/open-bus-api-client'
+import { GTFS_API } from './apiConfig'
 
-export interface Agency {
-  date: string // example - "2019-07-01"
-  operator_ref: number // example - 25,
-  agency_name: string // example - "אלקטרה אפיקים"
-}
-
-let agencyList: Agency[]
+let agencyListPromise: Promise<GtfsAgencyPydanticModel[]> | null = null
 
 const tryDates = [
-  dayjs().subtract(1, 'day').format('YYYY-MM-DD'),
-  dayjs().subtract(8, 'day').format('YYYY-MM-DD'),
-  '2025-05-18',
+  new Date(Date.now() - 24 * 60 * 60 * 1000),
+  new Date(Date.now() - 7 * 24 * 60 * 60 * 1000),
+  new Date('2025-05-18'),
 ]
 
-/**
- * Fetch agency data from MOT api
- * @returns Agency data array, might contain DUPLICATE agencies with different `date` values
- */
-export default async function getAgencyList(): Promise<Agency[]> {
-  if (!agencyList) {
-    let data: Agency[] = []
-    for (const date of tryDates) {
-      try {
-        const response = await fetch(`${STRIDE_API_BASE_PATH}/gtfs_agencies/list?date_from=${date}`)
-        if (!response.ok) continue
-        data = (await response.json()) as Agency[]
-        if (data.length > 0) break
-      } catch (err) {
-        console.error(err)
-      }
+async function fetchAgencyList() {
+  let data: GtfsAgencyPydanticModel[] = []
+  for (const date of tryDates) {
+    try {
+      data = await GTFS_API.gtfsAgenciesListGet({ dateFrom: date })
+      if (data.length > 0) break
+    } catch (err) {
+      console.error('Error fetching agencies:', err)
     }
-    agencyList = data.filter(Boolean)
   }
-
-  return agencyList
+  return data.filter(Boolean)
 }
 
-export function useAgencyList() {
-  const [agencyList, setAgencyList] = useState<Agency[]>([])
-
-  useEffect(() => {
-    getAgencyList().then(setAgencyList).catch(console.log)
-  }, [])
-
-  return agencyList
+export function getAgencyList() {
+  if (!agencyListPromise) {
+    agencyListPromise = fetchAgencyList()
+  }
+  return agencyListPromise
 }
