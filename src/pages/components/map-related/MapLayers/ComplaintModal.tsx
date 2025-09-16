7ac9bc0ed0bd050c7ece55541c55ebc88a0b46b8
aviv <avivabachi@gmail.com>
2025-09-16 17:07:29 +0300

    fix: update backend API base path and refactor complaint form submission

--- src/pages/components/map-related/MapLayers/ComplaintModal.tsx
+++ src/pages/components/map-related/MapLayers/ComplaintModal.tsx
@@ -101,13 +101,13 @@ const ComplaintModal = ({ modalOpen = false, setModalOpen, position }: Complaint
   const handleSubmit = (values: ComplaintsSendPostRequestUserData) => {
     submitMutation.mutate({
       debug: true,
-      userData: values,
-      databusData: {
-        ...siriRide.data,
+      userData: {
+        ...values,
         timeEvent: times.timeEvent?.format('HH:mm'),
         waitFrom: times.waitFrom?.format('HH:mm'),
         waitTo: times.waitTo?.format('HH:mm'),
       },
+      databusData: siriRide.data,
     })
   }
 
@@ -138,116 +138,125 @@ const ComplaintModal = ({ modalOpen = false, setModalOpen, position }: Complaint
           </DialogTitle>
 
           <DialogContent>
-            <Form form={form} layout="vertical" onFinish={handleSubmit}>
-              <Form.Item
-                name="firstName"
-                label={t('first_name')}
-                rules={[{ required: true, pattern: /[א-ת]+/u }]}>
-                <Input maxLength={25} />
-              </Form.Item>
-
-              <Form.Item
-                name="lastName"
-                label={t('last_name')}
-                rules={[{ required: true, pattern: /[א-ת]+/u }]}>
-                <Input maxLength={25} />
-              </Form.Item>
-
-              <Form.Item
-                name="id"
-                label={t('id')}
-                rules={[
-                  { required: true, len: 9 },
-                  {
-                    validator: (_, value) => {
-                      return IDValidator(value as string)
-                        ? Promise.resolve()
-                        : Promise.reject(new Error('Invalid ID'))
-                    },
-                  },
-                ]}>
-                <Input maxLength={9} />
-              </Form.Item>
-
-              <Form.Item
-                name="email"
-                label={t('email')}
-                rules={[{ type: 'email', required: true }]}>
-                <Input />
-              </Form.Item>
-
-              <Form.Item name="phone" label={t('phone')} rules={[{ required: true }]}>
-                <Input maxLength={11} />
-              </Form.Item>
-
-              <Form.Item
-                name="complaintType"
-                label={t('complaint_type')}
-                rules={[{ required: true }]}>
-                <Select
-                  options={complaintTypes.map((c) => ({ value: c.value, label: t(c.label) }))}
-                />
-              </Form.Item>
-
-              <Grid container spacing={{ md: 3, xs: 0 }}>
-                <Grid size={{ xs: 12, md: 6 }}>
-                  <Form.Item name="from" label={t('from')}>
-                    <Input maxLength={90} />
-                  </Form.Item>
-                </Grid>
-
-                <Grid size={{ xs: 12, md: 6 }}>
-                  <Form.Item name="to" label={t('destination')}>
-                    <Input maxLength={90} />
-                  </Form.Item>
-                </Grid>
-              </Grid>
-
-              <Grid container spacing={{ md: 3, xs: 0 }}>
-                <Grid size={{ xs: 12, md: 4 }}>
-                  <Form.Item name="eventTime" label="Event Time" rules={[{ required: true }]}>
-                    <TimePicker
-                      style={{ width: '100%' }}
-                      value={times.timeEvent}
-                      onChange={(time) => setTimes((times) => ({ ...times, timeEvent: time }))}
-                      format="HH:mm"
-                    />
-                  </Form.Item>
-                </Grid>
-
-                <Grid size={{ xs: 12, md: 8 }}>
-                  <Form.Item name="wait" label="Wait" rules={[{ required: true }]}>
-                    <TimePicker.RangePicker
-                      style={{ width: '100%' }}
-                      value={[times.waitFrom, times.waitTo]}
-                      onChange={(changeTimes) =>
-                        setTimes((times) => ({
-                          ...times,
-                          waitFrom: changeTimes?.[0] || null,
-                          waitTo: changeTimes?.[1] || null,
-                        }))
-                      }
-                      format="HH:mm"
-                    />
-                  </Form.Item>
-                </Grid>
-              </Grid>
-
-              <Form.Item
-                name="description"
-                label={t('description')}
-                rules={[{ required: true, min: 2 }]}>
-                <Input.TextArea rows={4} maxLength={1500} />
-              </Form.Item>
-
-              <DialogActions sx={{ justifyContent: 'flex-end', padding: 0 }}>
-                <Form.Item>
-                  <Button type="primary" htmlType="submit" onClick={() => form.submit()}>
-                    {t('submit_complaint')}
-                  </Button>
+            {submitMutation.isSuccess ? (
+              <div>
+                <Typography variant="h2">
+                  {`Your Complint Number: ${submitMutation.data.referenceNumber}`}
+                </Typography>
+                <Button onClick={() => setModalOpen?.(false)}>Close</Button>
+              </div>
+            ) : (
+              <Form form={form} layout="vertical" onFinish={handleSubmit}>
+                <Form.Item
+                  name="firstName"
+                  label={t('first_name')}
+                  rules={[{ required: true, pattern: /[א-ת]+/u }]}>
+                  <Input maxLength={25} />
                 </Form.Item>
-              </DialogActions>
-            </Form>
+
+                <Form.Item
+                  name="lastName"
+                  label={t('last_name')}
+                  rules={[{ required: true, pattern: /[א-ת]+/u }]}>
+                  <Input maxLength={25} />
+                </Form.Item>
+
+                <Form.Item
+                  name="id"
+                  label={t('id')}
+                  rules={[
+                    { required: true, len: 9 },
+                    {
+                      validator: (_, value) => {
+                        return IDValidator(value as string)
+                          ? Promise.resolve()
+                          : Promise.reject(new Error('Invalid ID'))
+                      },
+                    },
+                  ]}>
+                  <Input maxLength={9} />
+                </Form.Item>
+
+                <Form.Item
+                  name="email"
+                  label={t('email')}
+                  rules={[{ type: 'email', required: true }]}>
+                  <Input />
+                </Form.Item>
+
+                <Form.Item name="phone" label={t('phone')} rules={[{ required: true }]}>
+                  <Input maxLength={11} />
+                </Form.Item>
+
+                <Form.Item
+                  name="complaintType"
+                  label={t('complaint_type')}
+                  rules={[{ required: true }]}>
+                  <Select
+                    options={complaintTypes.map((c) => ({ value: c.value, label: t(c.label) }))}
+                  />
+                </Form.Item>
+
+                <Grid container spacing={{ md: 3, xs: 0 }}>
+                  <Grid size={{ xs: 12, md: 6 }}>
+                    <Form.Item name="from" label={t('from')}>
+                      <Input maxLength={90} />
+                    </Form.Item>
+                  </Grid>
+
+                  <Grid size={{ xs: 12, md: 6 }}>
+                    <Form.Item name="to" label={t('destination')}>
+                      <Input maxLength={90} />
+                    </Form.Item>
+                  </Grid>
+                </Grid>
+
+                <Grid container spacing={{ md: 3, xs: 0 }}>
+                  <Grid size={{ xs: 12, md: 4 }}>
+                    <Form.Item name="eventTime" label="Event Time" rules={[{ required: true }]}>
+                      <TimePicker
+                        style={{ width: '100%' }}
+                        value={times.timeEvent}
+                        onChange={(time) => setTimes((times) => ({ ...times, timeEvent: time }))}
+                        format="HH:mm"
+                      />
+                    </Form.Item>
+                  </Grid>
+
+                  <Grid size={{ xs: 12, md: 8 }}>
+                    <Form.Item name="wait" label="Wait" rules={[{ required: true }]}>
+                      <TimePicker.RangePicker
+                        style={{ width: '100%' }}
+                        value={[times.waitFrom, times.waitTo]}
+                        onChange={(changeTimes) =>
+                          setTimes((times) => ({
+                            ...times,
+                            waitFrom: changeTimes?.[0] || null,
+                            waitTo: changeTimes?.[1] || null,
+                          }))
+                        }
+                        format="HH:mm"
+                      />
+                    </Form.Item>
+                  </Grid>
+                </Grid>
+
+                <Form.Item
+                  name="description"
+                  label={t('description')}
+                  rules={[{ required: true, min: 2 }]}>
+                  <Input.TextArea rows={4} maxLength={1500} />
+                </Form.Item>
+
+                <DialogActions sx={{ justifyContent: 'flex-end', padding: 0 }}>
+                  <Form.Item>
+                    <Button type="primary" htmlType="submit" onClick={() => form.submit()}>
+                      {t('submit_complaint')}
+                    </Button>
+                  </Form.Item>
+                </DialogActions>
+              </Form>
+            )}
           </DialogContent>
         </Dialog>
       )}
