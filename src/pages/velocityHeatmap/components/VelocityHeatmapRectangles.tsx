49776e17a57d784db962d393ab56ad82988b4bb3
Noam Gaash <noam.gaash@applitools.com>
2025-09-23 19:49:29 +0300

    feat: enhance getValue and getRedOpacityColor functions for improved data handling and visualization

--- src/pages/velocityHeatmap/components/VelocityHeatmapRectangles.tsx
+++ src/pages/velocityHeatmap/components/VelocityHeatmapRectangles.tsx
@@ -9,7 +9,11 @@ function getValue(point: VelocityAggregation, visMode: VisMode): number {
   if (visMode === 'avg') return point.average_rolling_avg
   if (visMode === 'std') return point.stddev_rolling_avg
   if (visMode === 'cv') {
-    return point.stddev_rolling_avg > 0 ? point.average_rolling_avg / point.stddev_rolling_avg : 0
+    const MAX_CV = 19.99
+    return Math.min(
+      point.stddev_rolling_avg > 0 ? point.average_rolling_avg / point.stddev_rolling_avg : 0,
+      MAX_CV,
+    )
   }
   return 0
 }
@@ -18,8 +22,9 @@ function getRedOpacityColor(value: number, minV = 0, maxV = 1): string {
   // Clamp and normalize value to [0,1]
   // 0 = fully transparent, 1 = solid red
   if (maxV === minV) return 'rgba(255,0,0,0)'
-  const maxOpacity = 0.4
-  const norm = (1 - Math.max(0, Math.min(1, (value - minV) / (maxV - minV)))) * maxOpacity
+  const maxOpacity = 0.9
+  const norm = Math.max(0, Math.min(1, (value - minV) / (maxV - minV))) * maxOpacity
+  // norm = norm ** 2 / maxOpacity ** 2
   return `rgba(255,0,0,${norm})`
 }
 
@@ -71,9 +76,13 @@ export const VelocityHeatmapRectangles: React.FC<
           <Rectangle
             key={idx}
             bounds={bounds}
-            pathOptions={{ color: color, weight: 1, fillColor: color, fillOpacity: 1 }}>
+            pathOptions={{ weight: 1, fillColor: color, fillOpacity: 1, stroke: false }}>
             <Popup>
               <VelocityHeatmapPopup point={point} />
+              {color} <br />
+              value {value} <br />
+              minv {minV} <br />
+              maxv {maxV} <br />
             </Popup>
           </Rectangle>
         )
