64f4095a9ace879b4fcb7a5271c06087d3622441
Noam Gaash <noam.gaash@applitools.com>
2025-11-22 13:41:21 +0200

    refactor: simplify expect usage and remove custom duplication check

--- src/test_pages/TimelinePage.ts
+++ src/test_pages/TimelinePage.ts
@@ -1,20 +1,6 @@
 import type { Page } from '@playwright/test'
-import { test as base, expect as baseExpect } from 'tests/utils'
+import { test as base, expect } from 'tests/utils'
 import { BasePage } from './BasePage'
-
-export const expect = baseExpect.extend({
-  toHaveDuplications(received: string[]) {
-    const dups = received.toSorted().filter((item, index, arr) => item === arr[index - 1])
-    const uniqueDups = [...new Set(dups)]
-    const pass = (uniqueDups.length === 0) === this.isNot
-    const expectation = this.isNot ? 'Expected no duplications' : 'Expected duplications'
-    const reality = this.isNot
-      ? `but found duplications: ${uniqueDups.join(', ')}`
-      : 'but none were found.'
-    const message = () => `${expectation}, ${pass ? 'and' : 'but'} ${reality}`
-    return { pass, message, expected: [], actual: uniqueDups }
-  },
-})
 
 class TimelinePage extends BasePage {
   constructor(page: Page) {
