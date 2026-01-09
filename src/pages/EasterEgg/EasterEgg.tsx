9d5f5d5bf1e520129ea1e19f3f185811e5e37d79
aviv <avivabachi@gmail.com>
2026-01-09 12:53:31 +0200

    enhance bug report messages

--- src/pages/EasterEgg/EasterEgg.tsx
+++ src/pages/EasterEgg/EasterEgg.tsx
@@ -3,19 +3,32 @@ import useKonami from 'use-konami'
 
 export const FadeContext = createContext(false)
 
-export function EasterEgg({ children, code }: { children?: React.ReactNode; code: string }) {
+export function EasterEgg({
+  children,
+  code,
+  onShow,
+  autohide = true,
+}: {
+  children?: React.ReactNode
+  code: string
+  autohide?: boolean
+  onShow?: () => void
+}) {
   const [show, setShow] = useState(false)
   const [fade, setFade] = useState(false)
   useKonami({
     onUnlock: () => {
       setShow(true)
-      setTimeout(() => {
-        setFade(true)
-      }, 9000)
-      setTimeout(() => {
-        setShow(false)
-        setFade(false)
-      }, 10000)
+      onShow?.()
+      if (autohide) {
+        setTimeout(() => {
+          setFade(true)
+        }, 9000)
+        setTimeout(() => {
+          setShow(false)
+          setFade(false)
+        }, 10000)
+      }
     },
     sequence: code.split(''),
   })
