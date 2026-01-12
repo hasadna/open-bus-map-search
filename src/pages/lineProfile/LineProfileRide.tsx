f95a19ae8b8d405f22233ee1698fd06e55886c09
Aviv <AvivAbachi@gmail.com>
2026-01-12 11:04:48 +0200

    refactor: vehicleLocations  (#1413)

--- src/pages/lineProfile/LineProfileRide.tsx
+++ src/pages/lineProfile/LineProfileRide.tsx
@@ -1,32 +1,36 @@
+import type { SiriVehicleLocationWithRelatedPydanticModel } from '@hasadna/open-bus-api-client'
 import { useTranslation } from 'react-i18next'
 import dayjs from 'src/dayjs'
-import { VehicleLocation } from 'src/model/vehicleLocation'
 import Widget from 'src/shared/Widget'
 import { InfoItem, InfoTable } from '../components/InfoTable'
 import { vehicleIDFormat } from '../components/utils/rotueUtils'
 
-export const LineProfileRide = ({ point }: { point?: VehicleLocation }) => {
+export const LineProfileRide = ({
+  point,
+}: {
+  point?: SiriVehicleLocationWithRelatedPydanticModel
+}) => {
   const { t } = useTranslation()
 
   return (
     <Widget>
       <InfoTable>
-        <InfoItem label={t('lineProfile.ride.journey ')} value={point?.siri_ride__journey_ref} />
-        <InfoItem label={t('lineProfile.ride.id')} value={point?.siri_ride__id} />
-        <InfoItem label={t('vehicle_ref')} value={vehicleIDFormat(point?.siri_ride__vehicle_ref)} />
+        <InfoItem label={t('lineProfile.ride.journey ')} value={point?.siriRideJourneyRef} />
+        <InfoItem label={t('lineProfile.ride.id')} value={point?.siriRideId?.toString()} />
+        <InfoItem label={t('vehicle_ref')} value={vehicleIDFormat(point?.siriRideVehicleRef)} />
         <InfoItem
           label={t('lineProfile.ride.duration')}
           value={
-            point?.siri_ride__duration_minutes
-              ? `${point?.siri_ride__duration_minutes} ${t('minutes')}`
+            point?.siriRideDurationMinutes
+              ? `${point?.siriRideDurationMinutes} ${t('minutes')}`
               : undefined
           }
         />
         <InfoItem
           label={t('lineProfile.ride.scheduled')}
           value={
-            point?.siri_ride__scheduled_start_time
-              ? dayjs(point?.siri_ride__scheduled_start_time).format(t('datetime_format'))
+            point?.siriRideScheduledStartTime
+              ? dayjs(point?.siriRideScheduledStartTime).format(t('datetime_format'))
               : undefined
           }
         />
