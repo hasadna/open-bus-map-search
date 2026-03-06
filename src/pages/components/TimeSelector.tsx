6324c104b469da6e4046209fdc4c6bbcb9db90ca
Aviv <AvivAbachi@gmail.com>
2026-03-06 14:19:11 +0200

    feat: Click on gap cells to open single-line map for ride (#1448)

--- src/pages/components/TimeSelector.tsx
+++ src/pages/components/TimeSelector.tsx
@@ -2,14 +2,15 @@ import { renderTimeViewClock, TimePicker } from '@mui/x-date-pickers'
 import { useTranslation } from 'react-i18next'
 import { DataAndTimeSelectorProps } from './utils/dateAndTime'
 
-export function TimeSelector({ time, onChange }: DataAndTimeSelectorProps) {
+export function TimeSelector({ time, onChange, customLabel, minDate }: DataAndTimeSelectorProps) {
   const { t } = useTranslation()
   return (
     <TimePicker
       sx={{ width: '100%' }}
-      label={t('choose_time')}
+      label={customLabel || t('choose_time')}
       value={time}
       onChange={(ts) => onChange(ts)}
+      minTime={minDate}
       ampm={false}
       viewRenderers={{
         hours: renderTimeViewClock,
