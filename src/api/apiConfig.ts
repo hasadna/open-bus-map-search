15958a0b54020076a8c6218df78ecce0b3ca16a7
aviv <avivabachi@gmail.com>
2026-03-15 11:52:45 +0200

    update

--- src/api/apiConfig.ts
+++ src/api/apiConfig.ts
@@ -10,7 +10,7 @@ import {
   UserCasesApi,
 } from '@hasadna/open-bus-api-client'
 
-export const STRIDE_API_BASE_PATH = import.meta.env.VITE_STRIDE_API
+export const STRIDE_API_BASE_PATH = process.env.VITE_STRIDE_API
 const STRIDE_API_CONFIG = new Configuration({ basePath: STRIDE_API_BASE_PATH })
 
 export const AGGREGATIONS_API = new AggregationsApi(STRIDE_API_CONFIG)
@@ -18,7 +18,7 @@ export const GTFS_API = new GtfsApi(STRIDE_API_CONFIG)
 export const SIRI_API = new SiriApi(STRIDE_API_CONFIG)
 export const USER_CASE_API = new UserCasesApi(STRIDE_API_CONFIG)
 
-const BACKEND_API_BASE_PATH = import.meta.env.VITE_BACKEND_API
+const BACKEND_API_BASE_PATH = process.env.VITE_BACKEND_API
 const BACKEND_API_CONFIG = new Configuration({ basePath: BACKEND_API_BASE_PATH })
 
 export const HEALTH_API = new HealthApi(BACKEND_API_CONFIG)
