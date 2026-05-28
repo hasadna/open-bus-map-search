3c79ee9bdc9f32f2983ba838e96f10d02f071503
Eran <eran132@users.noreply.github.com>
2026-05-28 08:40:15 +0300

    feat: restyle velocity heatmap page (legend card, MUI controls, i18n) (#1526)

--- src/pages/velocityHeatmap/index.tsx
+++ src/pages/velocityHeatmap/index.tsx
@@ -1,26 +1,30 @@
 import { OpenInFullRounded } from '@mui/icons-material'
-import { IconButton, Stack } from '@mui/material'
+import { FormControlLabel, IconButton, Radio, RadioGroup, Stack, Typography } from '@mui/material'
 import React, { useCallback, useContext, useRef, useState } from 'react'
-import { MapContainer, TileLayer } from 'react-leaflet'
+import { useTranslation } from 'react-i18next'
+import { AttributionControl, MapContainer, TileLayer, ZoomControl } from 'react-leaflet'
 import dayjs from 'src/dayjs'
 import { useConstrainedFloatingButton } from 'src/hooks/useConstrainedFloatingButton'
 import { SearchContext } from '../../model/pageState'
 import { DateNavigator } from '../components/dateNavigator/DateNavigator'
 import { DateSelector } from '../components/DateSelector'
+import { PageContainer } from '../components/PageContainer'
 import { VelocityHeatmapLegend } from './components/VelocityHeatmapLegend'
 import { VelocityHeatmapRectangles } from './components/VelocityHeatmapRectangles'
 
 const VIS_MODES = [
-  { key: 'avg', label: 'Visualize Avg Speed' },
-  { key: 'std', label: 'Visualize Std' },
-  { key: 'cv', label: 'Visualize Std / Avg Speed (Coeff of Var)' },
-]
+  { key: 'avg', labelKey: 'velocity_vis_avg' },
+  { key: 'std', labelKey: 'velocity_vis_std' },
+  { key: 'cv', labelKey: 'velocity_vis_cv' },
+] as const
 
 const DEFAULT_ZOOM_LEVEL = 10
 
 const VelocityHeatmapPage: React.FC = () => {
   const [isExpanded, setIsExpanded] = useState<boolean>(false)
   const toggleExpanded = useCallback(() => setIsExpanded((expanded) => !expanded), [])
+  const { t, i18n } = useTranslation()
+  const isRtl = i18n.dir() === 'rtl'
 
   const { search, setSearch } = useContext(SearchContext)
 
@@ -38,30 +42,30 @@ const VelocityHeatmapPage: React.FC = () => {
   useConstrainedFloatingButton(mapContainerRef, buttonRef, isExpanded)
 
   return (
-    <div>
-      <h1>Velocity Aggregation Heatmap</h1>
-      <p>This page will display a heatmap of velocity aggregation data.</p>
+    <PageContainer>
+      <Typography variant="h4" component="h1" gutterBottom>
+        {t('velocity_heatmap_page_title')}
+      </Typography>
 
-      {/* choose date*/}
-      <Stack direction="column" spacing={2} sx={{ mb: 2, width: { xs: '100%', md: '70%' } }}>
+      <Stack direction="column" spacing={2} sx={{ maxWidth: 600 }}>
         <DateSelector time={dayjs(search.timestamp)} onChange={handleTimestampChange} />
         <DateNavigator currentTime={dayjs(search.timestamp)} onChange={handleTimestampChange} />
       </Stack>
-      <div style={{ margin: '12px 0' }}>
-        <b>Visualization:</b>{' '}
+      <RadioGroup
+        row
+        name="visMode"
+        value={visMode}
+        onChange={(e) => setVisMode(e.target.value as 'avg' | 'std' | 'cv')}
+        sx={{ flexWrap: 'wrap', mt: 2 }}>
         {VIS_MODES.map((mode) => (
-          <label key={mode.key} style={{ marginRight: 12 }}>
-            <input
-              type="radio"
-              name="visMode"
-              value={mode.key}
-              checked={visMode === mode.key}
-              onChange={() => setVisMode(mode.key as 'avg' | 'std' | 'cv')}
-            />{' '}
-            {mode.label}
-          </label>
+          <FormControlLabel
+            key={mode.key}
+            value={mode.key}
+            control={<Radio size="small" />}
+            label={t(mode.labelKey)}
+          />
         ))}
-      </div>
+      </RadioGroup>
 
       <div ref={mapContainerRef} className={`map-info ${isExpanded ? 'expanded' : 'collapsed'}`}>
         <IconButton
@@ -75,7 +79,11 @@ const VelocityHeatmapPage: React.FC = () => {
           center={[29.65, 34.6]}
           zoom={DEFAULT_ZOOM_LEVEL}
           scrollWheelZoom={true}
+          zoomControl={false}
+          attributionControl={false}
           style={{ height: '100%', width: '100%' }}>
+          <ZoomControl position={isRtl ? 'topleft' : 'topright'} />
+          <AttributionControl position={isRtl ? 'bottomright' : 'bottomleft'} prefix={false} />
           <TileLayer
             attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
             url="https://tile-a.openstreetmap.fr/hot/{z}/{x}/{y}.png"
@@ -90,7 +98,7 @@ const VelocityHeatmapPage: React.FC = () => {
           <VelocityHeatmapLegend visMode={visMode} min={min} max={max} />
         </MapContainer>
       </div>
-    </div>
+    </PageContainer>
   )
 }
 
