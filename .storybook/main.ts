a1556c81e5030d52a25530bed638d00acab4446b
arielvino <71963953+arielvino@users.noreply.github.com>
2026-05-29 10:45:55 +0300

    bump storybook

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,11 +1,13 @@
+import { fileURLToPath } from "node:url";
+import { dirname } from "node:path";
 import type { StorybookConfig } from '@storybook/react-vite'
 
 const config: StorybookConfig = {
   stories: ['../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
   staticDirs: ['../public'],
-  addons: ['@storybook/addon-docs'],
+  addons: [getAbsolutePath("@storybook/addon-docs")],
   framework: {
-    name: '@storybook/react-vite',
+    name: getAbsolutePath("@storybook/react-vite"),
     options: {},
   },
   typescript: {
@@ -19,3 +21,7 @@ export const getPastDate = () => {
 }
 
 export default config
+
+function getAbsolutePath(value: string): any {
+  return dirname(fileURLToPath(import.meta.resolve(`${value}/package.json`)));
+}
