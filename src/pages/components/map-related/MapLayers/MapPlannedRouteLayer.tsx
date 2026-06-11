5e682b36411026ef43c17ebebaf79b79f861915d
arielvino <71963953+arielvino@users.noreply.github.com>
2026-06-11 11:22:24 +0300

    make stop-details a tooltip instead of a popup

--- src/pages/components/map-related/MapLayers/MapPlannedRouteLayer.tsx
+++ src/pages/components/map-related/MapLayers/MapPlannedRouteLayer.tsx
@@ -1,4 +1,5 @@
-import { Marker, Polyline, Popup } from 'react-leaflet'
+import { useTranslation } from 'react-i18next'
+import { Marker, Polyline, Tooltip } from 'react-leaflet'
 import type { BusStop } from 'src/model/busStop'
 import { plannedRouteLineColor, plannedRouteStopMarker } from '../MapContent'
 
@@ -7,6 +8,8 @@ interface MapPlannedRouteLayerProps {
 }
 
 export function MapPlannedRouteLayer({ plannedRouteStops = [] }: MapPlannedRouteLayerProps) {
+  const { t } = useTranslation()
+
   return (
     <>
       <Polyline
@@ -17,27 +20,21 @@ export function MapPlannedRouteLayer({ plannedRouteStops = [] }: MapPlannedRoute
         ])}
       />
 
-      {plannedRouteStops.map((stop) => {
-        const totalMinutes = stop.minutesFromRouteStartTime
-        const h = Math.floor(totalMinutes / 60)
-        const m = totalMinutes % 60
-        const arrivalLabel = `${String(h).padStart(2, '0')}:${String(m).padStart(2, '0')} from start`
-
-        return (
-          <Marker
-            key={stop.key}
-            position={[stop.location.latitude, stop.location.longitude]}
-            icon={plannedRouteStopMarker}>
-            <Popup>
-              <strong>{stop.name}</strong>
-              <br />
-              Stop {stop.code} · #{stop.stopSequence}
-              <br />
-              {arrivalLabel}
-            </Popup>
-          </Marker>
-        )
-      })}
+      {plannedRouteStops.map((stop) => (
+        <Marker
+          key={stop.key}
+          position={[stop.location.latitude, stop.location.longitude]}
+          icon={plannedRouteStopMarker}>
+          <Tooltip direction="top" offset={[0, -12]}>
+            <strong>{stop.name}</strong>
+            <br />
+            {t('lineProfile.stop.code')}: {stop.code} · {stop.stopSequence}{' '}
+            {t('lineProfile.stop.of')} {plannedRouteStops.length}
+            <br />
+            {t('minutes_from_route_start', { minutes: stop.minutesFromRouteStartTime })}
+          </Tooltip>
+        </Marker>
+      ))}
     </>
   )
 }
