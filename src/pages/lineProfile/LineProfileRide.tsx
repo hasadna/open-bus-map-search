8df3d0ff84a59547253b09ea2619cf0ac7e5826f
Amit Steinmetz <amitsteinmetz@gmail.com>
2026-06-26 17:45:26 +0300

    adding table to represent all the vehicles data

--- src/pages/lineProfile/LineProfileRide.tsx
+++ src/pages/lineProfile/LineProfileRide.tsx
@@ -1,45 +1,46 @@
+import { Table, TableBody, TableCell, TableContainer, TableHead, TableRow } from '@mui/material'
 import { useTranslation } from 'react-i18next'
-import dayjs from 'src/dayjs'
 import type { PositionGroup } from 'src/pages/components/map-related/map-types'
 import Widget from 'src/shared/Widget'
-import { InfoItem, InfoTable } from '../components/InfoTable'
 import { vehicleIDFormat } from '../components/utils/rotueUtils'
 
 export const LineProfileRide = ({ positionGroups }: { positionGroups: PositionGroup[] }) => {
   const { t } = useTranslation()
-  const point = positionGroups[0]?.positions[0]?.point
-  const vehiclePlates = positionGroups
-    .map(
-      (group) =>
-        group.label ??
-        vehicleIDFormat(group.vehicleRef ?? group.positions[0]?.point?.siriRideVehicleRef),
-    )
-    .filter(Boolean)
-    .join(', ')
 
   return (
     <Widget>
-      <InfoTable>
-        <InfoItem label={t('lineProfile.ride.journey')} value={point?.siriRideJourneyRef} />
-        <InfoItem label={t('lineProfile.ride.id')} value={point?.siriRideId?.toString()} />
-        <InfoItem label={t('vehicle_refs')} value={vehiclePlates || undefined} />
-        <InfoItem
-          label={t('lineProfile.ride.duration')}
-          value={
-            point?.siriRideDurationMinutes
-              ? `${point?.siriRideDurationMinutes} ${t('minutes')}`
-              : undefined
-          }
-        />
-        <InfoItem
-          label={t('lineProfile.ride.scheduled')}
-          value={
-            point?.siriRideScheduledStartTime
-              ? dayjs(point?.siriRideScheduledStartTime).format(t('datetime_format'))
-              : undefined
-          }
-        />
-      </InfoTable>
+      <TableContainer>
+        <Table size="small">
+          <TableHead>
+            <TableRow>
+              <TableCell>{t('lineProfile.ride.journey')}</TableCell>
+              <TableCell>{t('lineProfile.ride.id')}</TableCell>
+              <TableCell>{t('vehicle_ref')}</TableCell>
+              <TableCell>{t('lineProfile.ride.duration')}</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {positionGroups.map((group, index) => {
+              const point = group.positions[0]?.point
+              const plate =
+                group.label ?? vehicleIDFormat(group.vehicleRef ?? point?.siriRideVehicleRef)
+
+              return (
+                <TableRow key={point?.siriRideId ?? index}>
+                  <TableCell>{point?.siriRideJourneyRef ?? '-'}</TableCell>
+                  <TableCell>{point?.siriRideId?.toString() ?? '-'}</TableCell>
+                  <TableCell>{plate ?? '-'}</TableCell>
+                  <TableCell>
+                    {point?.siriRideDurationMinutes
+                      ? `${point.siriRideDurationMinutes} ${t('minutes')}`
+                      : '-'}
+                  </TableCell>
+                </TableRow>
+              )
+            })}
+          </TableBody>
+        </Table>
+      </TableContainer>
     </Widget>
   )
 }
