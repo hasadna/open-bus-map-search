24974c8434d9b3b65a49e93ec54b57fc8947e952
arielvino <71963953+arielvino@users.noreply.github.com>
2026-08-06 23:02:26 +0300

    remove unused css

--- src/layout/sidebar/SideBar.tsx
+++ src/layout/sidebar/SideBar.tsx
@@ -1,5 +1,4 @@
 import { Drawer, Layout } from 'antd'
-import cn from 'classnames'
 import { useContext, useState } from 'react'
 import { useTranslation } from 'react-i18next'
 import { LayoutContextInterface, LayoutCtx } from '../LayoutContext'
@@ -23,7 +22,7 @@ export default function SideBar() {
         size={280}
         onClose={() => setDrawerOpen(false)}
         open={drawerOpen}
-        rootClassName={cn('hideOnDesktop', { dark: isDarkTheme })}
+        rootClassName="hideOnDesktop"
         styles={{ body: { padding: '0' } }}>
         <Menu />
       </Drawer>
@@ -40,7 +39,7 @@ export default function SideBar() {
           boxShadow: isDarkTheme ? '0 0 12px 4px rgba(0,0,0,0.7)' : '0 0 12px 4px rgba(0,0,0,0.12)',
         }}
         onCollapse={setCollapsed}
-        className={cn('hideOnMobile', { dark: isDarkTheme })}>
+        className="hideOnMobile">
         <Menu collapsed={collapsed} compact />
       </Sider>
     </>
